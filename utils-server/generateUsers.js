707fc6f90e9325761453f8dbd12bca7995d85e18
tuanhm <hoangminhtuan.hust@gmail.com>
2016-02-26 14:28:57 +0700

    26/2/2016 up code generate users, logs

---
+++ utils-server/generateUsers.js
@@ -0,0 +1,74 @@
+var MongoClient = require('mongodb').MongoClient,
+	url = 'mongodb://localhost:1234/test',
+	n = 20000,
+	collection = 'users';
+
+function generateDB(){
+	MongoClient.connect(url)
+	  	.then(function(db){
+		    console.log('[MongoDB] connected');
+		    var count = 0;
+		    for(var i=0;i<n;i++){
+		    	db.collection(collection).insertOne(generateUsers())
+		    		.then(function(results){
+		    			count++;
+		    			console.log(count + ' records');
+		    			if(count == n){
+		    				db.close();
+		    				console.log('Done');
+		    			}
+		    		}).catch(function(err){
+		    			console.log('[MongoDB] insert err', err.message);
+		    		});
+		    }
+		    
+		    // Listen for some events
+		    db.on('reconnect', function(data){
+		      console.log(data);
+		      console.log('[MongoDB] reconnect success');
+		    });
+		    db.on('error', function(err){
+		      console.log('[MongoDB] error', err.message);
+		    });
+		    db.on('close', function(err){
+		      console.log('[MongoDB] disconnected');
+		    });
+	  	}).catch(function(err){
+	    	console.error("[MongoDB]", err.message);
+	    	setTimeout(generateDB, 2000);
+	  	});	
+}
+
+
+function generateUsers(){
+	return {
+		appid: generateNumber(1, 4),
+		name: generateName(),
+		height: generateNumber(150, 180),
+		iq: generateNumber(30, 40),
+		age: generateNumber(20, 60),
+		gender: generateNumber(1,2) == 1? 'male':'female'
+	};
+}
+
+function generateNumber(min, max){
+	var range = max - min;
+	var temp = range.toString();
+	var str = "1";
+	for(var i=0;i<temp.length;i++){
+		str += '0';
+	}
+	temp = parseInt(str);
+
+	var number = Math.floor((Math.random()*temp)/(temp/(range+1)))+min;
+	return number;
+}
+
+function generateName(){
+	var ho =['An','Ánh','Ân','Âu','Ấu','Bá','Bạc','Bạch','Bàng','Bành','Bảo','Bế','Bì','Biện','Bình','Bồ','Ca','Cái','Cam','Cao','Cát','Cầm','Cấn','Chế','Chiêm','Chu','Chung','Chương','Chử','Cổ','Cù','Cung','Cự','Dã','Danh','Diêm','Doãn','Diệp','Đàm','Đan','Đào','Đậu','Điền','Đinh','Đoàn','Đôn','Đồng','Đổng','Đới','Đường','Giả','Giao','Giang','Giáp','Hà','Hạ','Hàn','Hán','Hy','Hình','Hoa','Hồng','Hùng','Hứa','Kha','Khương','Khâu','Khoa','Khổng','Khu','Khuất','Khúc','Kiều','Kim','La','Lạc','Lại','Lâm','Lều','Liễu','Lò','Lục','Lư','Lã','Lương','Lưu','Ma','Mã','Mạc','Mạch','Mai','Mang','Mâu','Mẫn','Mộc','Ninh','Nhâm','Ngân','Nghiêm','Nghị','Ngụy','Nhữ','Nông','Ong','Ông','Phi','Phí','Phó','Phùng','Phương','Quản','Quách','Sầm','Sơn','Sử','Tạ','Tào','Tăng','Thạch','Thái','Thành','Thào','Thân','Thập','Thi','Thiều','Thịnh','Thôi','Tiêu','Tiếp','Tòng','Tô','Tôn','Tông','Tống','Trang','Trà','Trác','Triệu','Trịnh','Trình','Trưng','Trương','Từ','Ty','Uông','Văn','Vi','Viên','Vương','Xa','Yên','Ngọc','Liêu','Lỗ'];
+
+	var ten = ['Mỹ','Duệ','Tăng','Cường','Tráng','Liên','Huy','Phát','Bội','Hương','Linh','Nghi','Hàm','Tốn','Thuận','Vĩ','Vọng','Biểu','Khôn','Quang','Lương','Kiến','Ninh','Hòa','Thuật','Du','Hành','Suất','Nghĩa','Phương','Dưỡng','Di','Tương','Thực','Hảo','Cao','Túc','Thể','Vi','Tường','Tịnh','Hoài','Chiêm','Viễn','Ái','Cảnh','Ngưỡng','Mậu','Thanh','Kha','Nghiễm','Khác','Do','Trung','Ðạt','Liên','Trung','Tập','Cát','Ða','Diên','Hội','Phong','Hanh','Hiệp','Trùng','Phùng','Tuấn','Lãng','Nghi','Hậu','Lưu','Thành','Tú','Diệu','Diễn','Khánh','Thích','Phương','Huy','Từ','Thể','Dương','Quỳnh','Cẩm','Phu','Văn','Ái','Diệu','Dương','Bách','Chi','Quân','Phụ','Dực','Vạn','Diệp','Hiệu','Khuông','Tương'];
+	return ho[Math.floor(Math.random()*100) % ho.length] + ' ' + ten[Math.floor(Math.random()*100) % ten.length]
+}
+
+generateDB();
