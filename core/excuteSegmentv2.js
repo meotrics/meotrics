9f368182024c03b045fa66a1ab8c5699fddbaa28
Administrator <thanhpk@live.com>
2016-02-27 16:44:04 +0700

    build map reduce function from new query format

--- core/excuteSegmentv2.js
+++ core/excuteSegmentv2.js
@@ -1,5 +1,5 @@
 var testJson = [{
-  type: "purchase", f: "sum", field: "amount", operator: ">", value: 5,
+  type: "purchase", f: "avg", field: "amount", operator: ">", value: 5,
   conditions:  ["amount", ">", 2, "and", "price", "=", 40]
 },
 "and", {
@@ -15,7 +15,6 @@ var testJson = [{
 function buildConditionsCode(conditions){
   var i = 0;
   var code = "";
-
   while (i < conditions.length) {
     //get the next operator (if exist)
     var nextop = i + 3 < conditions.length ? conditions[i+3] : "";
@@ -27,16 +26,26 @@ function buildConditionsCode(conditions){
   return code;
 }
 
+function buildReduceChunk(ind, condition)
+{
+
+
+}
+
 //purpose: build a piece of map function
 //example: buildMapChunk({actiontype: "pageview", conditions: [{f: "count", field: "pid", operator: ">", value: 5, conditions: ["amount", ">", 5, "and", "price", "=", "dd"]})
 //return: string contains compiled javascript code
 //param: i=index of condition, for iteration purpose, condition=see example
-function buildMapChunk(ind, element){
+function buildChunk(ind, element){
+  var reducecondcode = "";
+  var reduceinitcode = "";
+  var reduceaggcode = ""
   var code="if(this.actiontype==='" + element.type + "'){";
   var conditions = element.conditions;
   var defvalcode = "";
-
+  var aggcode = "";
   //get the condition code
+  if(element.conditions === undefined) console.log(element);
   var inlineconditions = buildConditionsCode(element.conditions);
 
   //if the query is to count then just map 1, but if the query is to sum
@@ -44,38 +53,71 @@ function buildMapChunk(ind, element){
   if(element.f === "count" )
   {
     code += "value.f" + ind + "=(" + inlineconditions + ")?1:0;";
-    defvalcode += "value['f"+ ind +"']=0;";
+    reduceinitcode += "returnObject.f"+ ind +"=0;";
+    reduceaggcode += "returnObject.f" + ind + "+=value.f" + ind + ";";
+    defvalcode += "value.f"+ ind + "=0;";
+    reducecondcode += "(value.f" + ind + element.operator + JSON.stringify(element.value)+")";
   }
   else if(element.f === "sum"){
     code += "value.f" + ind + "=(" + inlineconditions + ")?this." + element.field + ":0;";
-    defvalcode += "value['f"+ ind +"']=0;";
+    reduceinitcode += "returnObject.f"+ ind +"=0;";
+    reduceaggcode += "returnObject.f" + ind + "+=value.f" + ind + ";";
+    defvalcode += "value.f"+ ind +"=0;";
+    reducecondcode += "(value.f" + ind + element.operator + JSON.stringify(element.value)+")";
   }
   else if(element.f === "avg")
   {
     code += "value.f" + ind + "_1=(" + inlineconditions + ")?this." + element.field + ":0;";
     code += "value.f" + ind + "_2=(" + inlineconditions + ")?1:0;";
-
-    defvalcode += "value['f"+ ind +"_1']=0;";
-    defvalcode += "value['f"+ ind +"_2']=0;";
+    reduceinitcode += "returnObject.f"+ ind +"_1=0;";
+    reduceaggcode += "returnObject.f" + ind + "_1+=value.f" + ind + "_1;";
+    reduceinitcode += "returnObject.f"+ ind +"_2=0;";
+    reduceaggcode += "returnObject.f" + ind + "_2+=value.f" + ind + "_2;";
+    defvalcode += "value.f"+ ind +"_1=0;";
+    defvalcode += "value.f"+ ind +"_2=0;";
+    reducecondcode += "(1.0*value.f" + ind + "_1/value.f" + ind +"_2" + element.operator + JSON.stringify(element.value)+")";
   }
   else throw "wrong operator";
 
-  code = defvalcode + code + "};\\n";
-  return code;
+  code = defvalcode + code + "};";
+  return {
+    mapcode: code,
+    reduceinitcode:reduceinitcode,
+    reducecondcode:reducecondcode,
+    reduceaggcode:reduceaggcode
+  };
 }
 
 //purpose: build a javascript map functino from a json query
 //example: buildMapFunction(testJson)
 //return: string contains compiled javascript code
 //param: query=see testJson
-function buildMapFunction(query){
-  var code="function(){var value={};";
+function buildMapReduce(query){
+  var mapfunccode="function(){var value={};";
+  var reducecondcode = "";
+  var reduceinitcode = "";
+  var reduceaggcode = "";
+
+
   var i = 0;
   while (i < query.length) {
-    console.log(i/2);
-    code += buildMapChunk(i/2, query[i]);
-    i+=2;
+    if(i%2==0)
+    {
+      console.log(i/2)
+      var tmpcode = buildChunk(i/2, query[i]);
+      mapfunccode += tmpcode.mapcode;
+      reduceinitcode += tmpcode.reduceinitcode;
+      reducecondcode += tmpcode.reducecondcode;
+      reduceaggcode += tmpcode.reduceaggcode;
+      console.log(reduceaggcode);
+    }
+    else{
+      reducecondcode += query[i];
+    }
+    i++;
   }
-  code += "}";
-  return code;
+  mapfunccode += "}";
+
+  var reducefunccode = "function(key,values){var returnObject={};" + reduceinitcode  +"for(var i in values){var value=values[i];"+reduceaggcode+"};return "+reducecondcode+"?key:null;}";
+  return {map:mapfunccode, reduce:reducefunccode };
 }
