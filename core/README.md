115ec19fcb21758e001a6cbfba23250defbc5342
Administrator <thanhpk@live.com>
2016-02-28 22:05:34 +0700

    add support for config

--- core/README.md
+++ core/README.md
@@ -1,1 +1,4 @@
 #API sử lý data cho meotrics
+
+Trước khi chạy, nhớ copy file config/production.json sang file config/default.json
+__Sửa config ở config/default.json, không sửa ở production.json__ 
--- core/app.js
+++ core/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var config = require('config');
+
 var app = express();
+
+
+var port = config.get("port");
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port +'!');
 });
