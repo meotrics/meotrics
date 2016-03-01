ae4a39483d3814fe2fd8e2df687f498f34392961
Administrator <thanhpk@live.com>
2016-03-01 23:49:57 +0700

    write function description

--- core/app.js
+++ core/app.js
@@ -69,39 +69,111 @@ if (cluster.isMaster) {
 		app.get('/trend', function (req, res) {
 
 		});
+	
+		//create or update a trend
+		app.post('/trend', function(req, res){
+			
+		});
 
+		//list trend
+		app.get('/trends', function(req,res)
+		{});
+		
+		app.delete('/trend', function(req,res)
+		{
+			
+		});
 
-
-
+		
+		app.post('/trend/draf', function(req, res)
+		{
+			
+		});
+		
 		//CLIENT------------------------------------------------------------------------
-		//record and event
-		app.post('/r', function recordAnEvent(req, res) {
-			//NOTE: use array instead of json for faster network throughput
-			// Store
+		
+		/* Ghi nhận một action mới
+		Tham số: {
+			appid : number, 
+			userid: number, 
+			mtid : number, 
+			typeid: number // type of action, eg: "purchase", "pageview"
+			osid: number // os information, eg: "window", "linux", 
+			browserid : number // eg, "chrome", "firefox", "opera", 
+			locationid : number // location code, eg: Hanoi, Vietnam 
+			referer: string
+			campaignid : number
+			deviceid : number
+			ctime: date // created time
+			ip: string // public ip address
+			screenres: string // screen resolution of the device
+			totalsec: number // total number of sec to finish the action
+			url: string
+			browserversion : number
+			osversion : number
+			field1, field2, field3, field4
+		}
+		*/
+		app.post('/r', function (req, res) {
+			
 		});
 
-		//identify an user
-		app.post('/i', function recordAnEvent(req, res) {
-			//NOTE: use array instead of json for faster network throughput
-			// Create new user for a app
-
-			// Update user in a app
-
-			// Identify an user
+		/* Phương thức này dùng để báo cho hệ thống biết một anonymous user thực ra là
+			một user đã tồn tại. Xem thêm ở http://pasteboard.co/1WAK4HYz.png
+			
+			Tham số:
+			{
+				appid: number,
+				mtid: number, //mtid của anonymous user
+				userid, name, email, age, birth, gender, ...
+			}
+			
+			Điều kiện: 
+			1. Toàn bộ actions thuộc anonymous user được sang tên cho user, thông tin về
+					user được cập nhập
+					Chú ý: nếu userid không tồn tại trong hệ thống, thì cập nhật luôn userid của anonymous
+					user thành userid ở tham số.
+			2. Toàn bộ thông tin về user được cập nhật mới.
+			*/
+		app.post('/i', function (req, res) {
 
 		});
 
-		//set up new cookie for new visitor
+		/* Thiết lập cookie mới cho người dùng mới
+		Tham số: {
+			appid: number
+		}
+		Điều kiện:
+		Một bản ghi user được tạo trong collection của appid, thông tin trống rỗng
+		Đầu ra:
+		mtid vừa được tạo
+		*/
 		app.post('/s', function (req, res) {
-			//NOTE: use array instead of json for faster network throughput
-			// appid
 			res.json({ uid: uuid.v4() });
 		});
 
-
-
-		//SEGMENTATION--------------------------------------------------------------
-
+		//SEGMENTATION-------------------------------------------------------------
+		
+		//make a segment query
+		app.get('segment', function(req, res)
+		{
+			
+		});
+		
+		//list segment
+		app.get('segments', function(req,res)
+		{});
+		
+		app.post('segment/draf', function(req, res)
+		{});
+		
+		//delete a segment
+		app.delete('segment', function(req, res)
+		{
+			
+		})
+		
+		//update or create a segment
 		app.post('segment', function (req, res) {
 			segmgr.create(req.params, function () {
 				res.send(200);
