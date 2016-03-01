0c079523e5daefa97d575d7a854a826c9286f37c
Administrator <thanhpk@live.com>
2016-03-01 16:59:44 +0700

    fix code error

--- core/test/testBarrier.js
+++ core/test/testBarrier.js
@@ -4,21 +4,19 @@ function simpletest() {
 	var b = new Barrier(2, function (param) {
 		console.log(param);
 	});
-	
-	function t1(){
-		setTimeout(function()
-		{
-			b.done({s1:1},{t1: 4});
-		},400);
+
+	function t1() {
+		setTimeout(function () {
+			b.done({ s1: 1 }, { t1: 4 });
+		}, 400);
 	}
-	
-	function t2(){
-		setTimeout( function()
-		{
-			b.done({s2:6, t2: 8});
-		},200);
+
+	function t2() {
+		setTimeout(function () {
+			b.done({ s2: 6, t2: 8 });
+		}, 200);
 	}
-	
+
 	t1();
 	t2();
 }
