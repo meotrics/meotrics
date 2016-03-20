4ad91f33234eb520e52e1014103afa5b38437f15
Administrator <thanhpk@live.com>
2016-03-21 02:20:38 +0700

    create widget for segment select

--- dashboard/resources/views/segment/index.blade.php
+++ dashboard/resources/views/segment/index.blade.php
@@ -24,14 +24,26 @@
 			data.push({type: 'prop', name: prop.name, dpname: prop.dpname});
 		}
 
-		$(function () {
-
+		onPageLoad(function () {
 			sq = new SegmentQuery();
 
 			sq.produce(function ($query) {
 				$('.id_query').append($query);
 
 			}, data);
+		});
+
+
+		$('.id_exebtn').click(function () {
+			$(this).attr('disabled', true);
+
+
+			var $field1 = $('.id_field1-43');
+			var $field2 = $('.id_field1-54');
+
+			$.get('/segment/execute', {id: -1, name: 'Draf', query: sq.query(), f1: $field1.val(), f2: $field2.val()}, function (data) {
+				console.log(data);
+			});
 
 		});
 
@@ -55,15 +67,15 @@
 			<div class="content row">
 				<div class="col-md-12">
 					<h5 class="mr5 mb">FILTER BY </h5>
-					<select class="fieldselect form-control mr5" style="width:150px; display: inline-block">
+					<select class="fieldselect form-control id_field1-43 mr5" style="width:150px; display: inline-block">
 						<option value="pid">Product ID</option>
 						<option value="cid">Category ID</option>
 					</select>
-					<select class="fieldselect form-control mr5" style="width:150px; display: inline-block">
+					<select class="fieldselect form-control id_field2-54 mr5" style="width:150px; display: inline-block">
 						<option value="pid">Product ID</option>
 						<option value="cid">Category ID</option>
 					</select>
-					<a class="btn btn-success btn-fill mr5"><i class="fa fa-bolt"></i> Excute</a>
+					<a class="btn btn-success btn-fill mr5 id_exebtn"><i class="fa fa-bolt"></i> Excute</a>
 					<a class="btn "><i class="fa fa-save"></i></a>
 				</div>
 			</div>
