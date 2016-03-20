47b81abe3937ce9f5b83d6975eb0c0094194ea19
Administrator <thanhpk@live.com>
2016-03-21 01:51:03 +0700

    fix routing

--- dashboard/app/Http/Controllers/SegmentController.php
+++ dashboard/app/Http/Controllers/SegmentController.php
@@ -1,8 +1,32 @@
 <?php namespace App\Http\Controllers;
 
+use App\Util\MtHttp;
+use Illuminate\Http\Request;
+
 class SegmentController extends Controller
 {
-	public function index()
+	public function getExecute(Request $request)
+	{
+		//get app id from cookie
+		$appid = 1; //Cookie::get('appid');
+		$query = $request->input('query');
+		$id = $request->input('id');
+
+
+		//Check if old segment
+		if ($id == null || $id == -1) {
+
+			$id = MtHttp::post('segment/' . $appid, ['query' => $query, 'name' => 'Draf']);
+		}
+
+
+		//run querry on field
+		//MtHttp::get(':3000/segment/query')
+
+		return json_encode([]);
+	}
+
+	public function getIndex()
 	{
 
 		$options = array(
