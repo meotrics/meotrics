2187b4b14d60e14e2ea0d54ccb7c45de92ce14b5
Administrator <thanhpk@live.com>
2016-03-22 22:53:35 +0700

    finish trend query

--- dashboard/app/Http/Controllers/TrendController.php
+++ dashboard/app/Http/Controllers/TrendController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Http\Controllers;
+
 use App\Util\MtHttp;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
-class TrendController extends Controller {
+class TrendController extends Controller
+{
 
 
 	public function __construct()
@@ -12,23 +14,58 @@ class TrendController extends Controller {
 		$this->middleware('auth');
 	}
 
-	public function index()
-	{
-		$actiontypes = MtHttp::get('actiontype/' . '1');
-		return view('trend/index', ['types' => json_encode($actiontypes)]);
-	}
-
-	public function query(Request $request)
+	public function getSave(Request $request)
 	{
 		$data = array(
-			'event' => $request->input('typeid') ,
+			'typeid' => $request->input('typeid'),
 			'object' => $request->input('object'),
 			'operation' => $request->input('operation'),
-			'param' => $request->input('param')
+			'param' => $request->input('param'),
+			'order' => intval($request->input('order')),
+			'name' => $request->input('name'),
 		);
+		return MtHttp::post('trend/1', $data);
+	}
 
-		$trendid = MtHttp::post('trend/1', $data);
-		$result =MtHttp::get('trend/query/1/' . $trendid);
+	public function getList()
+	{
+		return json_encode(MtHttp::get('trend/' . '1'));
+	}
+
+	public function getDelete()
+	{
+
+	}
+
+	public function getIndex()
+	{
+		$actiontypes = MtHttp::get('actiontype/' . '1');
+		$trends = MtHttp::get('trend/' . '1');
+		return view('trend/index', [
+			'types' => json_encode($actiontypes),
+			'trends' => json_encode($trends)
+		]);
+	}
+
+	public function getQuery(Request $request)
+	{
+		if ($request->input('_id') != null) {
+			$trendid = $request->input('_id');
+		} else {
+
+			$data = array(
+				'typeid' => $request->input('typeid'),
+				'object' => $request->input('object'),
+				'operation' => $request->input('operation'),
+				'param' => $request->input('param'),
+				'order' => intval($request->input('order')),
+				'name' => 'Draft',
+				'_isDraft' => true
+			);
+			$trendid = MtHttp::post('trend/1', $data);
+		}
+
+		$result = MtHttp::get('trend/query/1/' . $trendid);
 		return $result;
 	}
 
