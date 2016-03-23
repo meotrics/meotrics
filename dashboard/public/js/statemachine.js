37da9fdb6795d6fcb0ec741230163c2e61b6a8dc
Administrator <thanhpk@live.com>
2016-03-23 18:22:39 +0700

    support for query state

--- dashboard/public/js/statemachine.js
+++ dashboard/public/js/statemachine.js
@@ -1,25 +1,25 @@
-function StateMachine(_guid, transitions, handlers, refreshhandlers) {
+function StateMachine(_guid, handlers, refreshhandlers) {
 	var guid = "state-" + _guid;
 	var me = this;
-	//var currentstate; state-CB83A33D-756B-4353-AC4B-7DA5F6B05AC0-9FAFA321-A9EB-4291-BD37-3DEB73DF7631
-
 	var state;
 	//first time
 	if (localStorage[guid] == undefined) {
 		state = {};
-		state.current = state.last = "init";
+		me.current = me.last = state.current = state.last = "init";
 		localStorage[guid] = JSON.stringify(state);
 	}
 	else {
 		//page get refreshed
 		state = JSON.parse(localStorage[guild]);
+		me.current = state.current;
+		me.last = state.last;
 		refreshhandlers[state.current]();
 	}
 
 	this.switch = function (newstate) {
 		var state = localStorage[guid];
-		state.last = state.current;
-		state.current = newstate;
+		me.last = state.last = state.current;
+		me.current = state.current = newstate;
 		localStorage[guid] = JSON.stringify(state);
 		//call handler
 		handlers[[state.last, state.current]]();
