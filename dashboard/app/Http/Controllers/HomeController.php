5224fe33cc79a23cf42a083dab4fe8b3f213e270
Administrator <thanhpk@live.com>
2016-03-29 13:30:21 +0700

    add track function

--- dashboard/app/Http/Controllers/HomeController.php
+++ dashboard/app/Http/Controllers/HomeController.php
@@ -1,13 +1,15 @@
 <?php namespace App\Http\Controllers;
+
 use App\Util\MtHttp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use \Mobile_Detect;
 use UAParser\Parser;
 
 class HomeController extends Controller
 {
 
-	private static $parser;
+	//private  $parser;
 	/*
 	|--------------------------------------------------------------------------
 	| Home Controller
@@ -26,8 +28,8 @@ class HomeController extends Controller
 	 */
 	public function __construct()
 	{
-		HomeController::$parser = Parser::create();
-		$this->middleware('auth');
+		$this->parser = Parser::create();
+		//$this->middleware('auth');
 	}
 
 	/**
@@ -40,22 +42,76 @@ class HomeController extends Controller
 		return view('home');
 	}
 
-	public function pageView()
+	public function pageView(Request $request)
+	{
+		$req = $this->trackBasic($request);
+		$req['_type'] = 'pageview';
+		MtHttp::post('r', json_encode($req));
+		return '';
+	}
+
+	public function pageQuit(Request $request)
 	{
 	}
 
-	public function pageQuit()
+	private function getRemoteIPAddress(Request $request)
 	{
+		if (null != $request->ip()) return $request->ip();
+		if (!empty($_SERVER['HTTP_CLIENT_IP'])) return $_SERVER['HTTP_CLIENT_IP'];
+		if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) return $_SERVER['HTTP_X_FORWARDED_FOR'];
+		return $_SERVER['REMOTE_ADDR'];
+	}
+
+	private function trackBasic(Request $request)
+	{
+		$screenres = $request->input('_screenres', '');
+		$referrer = $request->input('_referrer', '');
+		$ip = $this->getRemoteIPAddress($request);
+
+		$type = $request->input('_type');
+
+		//browser, platform
+		$uas = $request->header('User-Agent');
+		$ua = $this->parser->parse($uas);
+
+		//device type
+		$detect = new Mobile_Detect;
+		if ($detect->isTablet($uas))
+			$devicetype = 'tablet';
+		else if ($detect->isMobile($uas))
+			$devicetype = 'tablet';
+		else
+			$devicetype = 'desktop';
+
+		//input data
+		$input = $request->all();
+		$data = [];
+		foreach ($input as $k => $v) {
+			if (substr($k, 0, 1) != '_')
+				$data[$k] = $v;
+		}
+
+		return [
+			'ip' => $ip,
+			'browserid' => $ua->ua->family,
+			'browserversion' => $ua->ua->major . "." . $ua->os->minor,
+			'osid' => $ua->os->family,
+			'osversion' => $ua->os->major . '.' . $ua->os->minor,
+			'deviceid' => $ua->device->family,
+			'devicetype' => $devicetype,
+			'referrer' => $referrer,
+			'data' => $data,
+			'screenres' => $screenres,
+			'url' => $request->server('HTTP_REFERER'),
+			'language' => $request->server('HTTP_ACCEPT_LANGUAGE')
+		];
 	}
 
 	public function track(Request $request)
 	{
-		$ua = $request->server('HTTP_USER_AGENT');
-		return $ua;
-		$ua = $request->header('User-Agent');
-		return $ua;
-		//$result = $parser->parse($ua)
-
+		$req = $this->trackBasic($request);
+		MtHttp::post('r', json_encode($req));
+		return '';
 	}
 
 	public function identify()
@@ -71,4 +127,5 @@ class HomeController extends Controller
 		$response->withCookie(cookie()->forever('mtid', $mtid));
 		return $response;
 	}
+
 }
