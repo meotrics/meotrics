f5806902b9706d8a50c57e911a1ec6ea8d140588
Administrator <thanhpk@live.com>
2016-04-08 18:57:15 +0700

    fix toIDs and objectid bug

--- core/module/trendmgr.js
+++ core/module/trendmgr.js
@@ -3,15 +3,12 @@ exports.TrendMgr = function (db, mongodb, async, converter, prefix, col) {
 	this.queryRaw = function (appid, trid, callback) {
 		var collection = prefix + col;
 		var results = [];
-
-		db.collection(collection).find({_id: new mongodb.ObjectID(trid)}, {_id: 0}).toArray(function (err, r) {
+		db.collection(collection).find({_id: new mongodb.ObjectID(trid)}, {_id: 0}).limit(1).next(function (err, trenddoc) {
 			if (err) throw err;
-			getQueryTrending(r[0], converter, function (query) {
+			getQueryTrending(trenddoc, converter, function (query) {
 				var collection = prefix + appid;
-				db.collection(collection).aggregate(query).toArray(function (err, array) {
+				db.collection(collection).aggregate(query).toArray(function (err, results) {
 					if (err) throw err;
-					results = array;
-
 					async.forEachOf(results, function (value, key, asyncCallback) {
 						converter.toOriginal(value.temp, function (r) {
 							results[key].temp = r;
@@ -19,7 +16,7 @@ exports.TrendMgr = function (db, mongodb, async, converter, prefix, col) {
 						});
 					}, function (err) {
 						if (err) throw err;
-						callback(null, results);
+						callback(results);
 					});
 				});
 			});
@@ -29,36 +26,38 @@ exports.TrendMgr = function (db, mongodb, async, converter, prefix, col) {
 	this.query = function (req, res) {
 		var appid = req.params.appid;
 		var trid = req.params.id;
-		me.queryRaw(appid, trid, function (err, results) {
+		me.queryRaw(appid, trid, function (results) {
 			res.json(results);
 		});
 	};
 
 	function getQueryTrending(object, converter, callback) {
 		var query = [];
-		converter.toID(object.object, function (r) {
-			object.object = r;
+		converter.toIDs([object.object, "_typeid", "_ctime", "_segment", object.param], function (ids) {
+			object.object = ids[object.object];
+			object.param = ids[object.param];
 
+			var match = {$match: {}};
+			match['$match'][ids["_typeid"]] = object.typeid;
 			// -- START MATCH CLAUSE
-			query.push({$match: {_typeid: new mongodb.ObjectID(object.typeid)}});
-
+			query.push(match);
 			if (object._segment != undefined) {
-				query[0]['$match']['_segments'] = {
+				match[ids['_segments']] = {
 					'$in': [object._segment]
 				};
 			}
 
 			if (object.startTime != undefined) {
-				query[0]['$match']['_ctime'] = {
+				match[ids['_ctime']] = {
 					$gte: object.startTime
 				};
 			}
 
 			if (object.endTime != undefined) {
-				if (query[0]['$match']['_ctime'] != undefined) {
-					query[0]['$match']['_ctime']['$lte'] = object.endTime;
+				if (match[ids['_ctime']] != undefined) {
+					match[ids['_ctime']]['$lte'] = object.endTime;
 				} else {
-					query[0]['$match']['_ctime'] = {
+					match[ids['_ctime']] = {
 						$lte: object.endTime
 					};
 				}
@@ -75,41 +74,33 @@ exports.TrendMgr = function (db, mongodb, async, converter, prefix, col) {
 				object.limit = object.limit || 10;
 				query.push({$limit: object.limit});
 
-				converter.toObject(query[0]['$match'], function (r) {
-					query[0]['$match'] = r;
-					callback(null, query);
-				});
+				callback(query);
 				return;
 			}
 
-			converter.toID(object.param, function (r) {
-				object.param = r;
+			if (object.operation == 'avg') {
+				query.push({
+					$group: {
+						_id: '$' + object.object,
+						result: {'$avg': '$' + object.param},
+						temp: {$first: "$$ROOT"}
+					}
+				});
+				query.push({$sort: {result: object.order}});
+			} else if (object.operation == 'sum') {
+				query.push({
+					$group: {
+						_id: '$' + object.object,
+						result: {'$sum': '$' + object.param},
+						temp: {$first: "$$ROOT"}
+					}
+				});
+				query.push({$sort: {result: object.order}});
+			}
+			object.limit = object.limit || 10;
+			query.push({$limit: object.limit});
 
-				if (object.operation == 'avg') {
-					query.push({
-						$group: {
-							_id: '$' + object.object,
-							result: {'$avg': '$' + object.param},
-							temp: {$first: "$$ROOT"}
-						}
-					});
-					query.push({$sort: {result: object.order}});
-				} else if (object.operation == 'sum') {
-					query.push({
-						$group: {
-							_id: '$' + object.object,
-							result: {'$sum': '$' + object.param},
-							temp: {$first: "$$ROOT"}
-						}
-					});
-					query.push({$sort: {result: object.order}});
-				}
-				object.limit = object.limit || 10;
-				query.push({$limit: object.limit});
-				query[0]['$match'] = converter.toObject(query[0]['$match']);
-				callback(query);
-			});
-
+			callback(query);
 		});
 	}
 };
--- core/test/testtrend.js
+++ core/test/testtrend.js
@@ -4,42 +4,32 @@ var MongoClient = require('mongodb').MongoClient;
 var config = require('config');
 var async = require('async');
 var mongodb = require('mongodb');
+var CRUD = require('../module/crud.js').CRUD;
 
 var converter = require('../utils/fakeidmanager.js');
-var converter = new converter.IdManager();
-function mtthrow(err) {
-	setTimeout(function () {
-		throw err;
-	});
-}
+converter = new converter.IdManager();
 
 var appid = 1;
 
 var prefix = config.get('mongod.prefix');
 MongoClient.connect("mongodb://" + config.get("mongod.host") + ":" + config.get('mongod.port') + '/' + config.get('mongod.database'), function (err, db) {
 	if (err) throw err;
-	var seg = new TrendMgr(db, mongodb, async, converter, prefix, mtthrow, appid);
+	var trend = new TrendMgr(db, mongodb, async, converter, prefix, "trend");
+	var trendCrud = new CRUD(db, mongodb, async, converter, prefix, "trend");
 
+	var t = {
+		operation: 'avg',
+		param: 'price',
+		object: 'pid',
+		typeid: 'purchase',
+		order: 1
+	};
 
-	var testJson2 = [{
-		type: "purchase",
-		f: "avg",
-		field: "quantity",
-		operator: ">",
-		value: 3,
-		conditions: []
-	}];
-
-	var segment = {
-		_id: 123,
-		query: testJson2,
-		appid: process.argv[2]
-	}
-
-	console.time('mr');
-	seg.runSegment(segment, function (out) {
-		console.timeEnd('mr');
-		console.log(JSON.stringify(out));
+	//create a trend
+	trendCrud.createRaw(appid, t, function (data) {
+		console.log(data);
+		trend.queryRaw(appid, data, function ( results) {
+			console.log(results);
+		});
 	});
-
 });
