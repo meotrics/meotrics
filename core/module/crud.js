bd17aa9d09cd09183b89b1c691dcb375b4ac2e1c
Administrator <thanhpk@live.com>
2016-04-14 01:27:42 +0700

    remove all promise

--- core/module/crud.js
+++ core/module/crud.js
@@ -1,25 +1,27 @@
 exports.CRUD = function (db, mongodb, async, converter, prefix, col) {
 	var me = this;
 
-	this.createRaw = function (appid, data, callback, next) {
+	this.createRaw = function (appid, data, callback) {
 		var collection = prefix + col;
 		data._appid = Number(appid);
 		data._ctime = Math.round(new Date() / 1000);
-		converter.toObject(data).then(function (r) {
-			return db.collection(collection).insertOne(r);
-		}).then(function (r) {
-			var _typeid = r.insertedId;
-			callback(_typeid);
-		}).catch(next);
+		converter.toObject(data, function (r) {
+			db.collection(collection).insertOne(r, function (err, ret) {
+				if (err) throw err;
+				var _typeid = ret.insertedId;
+				callback(_typeid);
+			});
+		});
 	};
 
-	this.create = function (req, res, next) {
-		me.createRaw(req.params.appid, req.body, function (typeid) {
-			res.send(typeid);
-		}, next);
+	this.create = function (req, res, callback) {
+		me.createRaw(req.params.appid, req.body, function (id) {
+			res.send(id);
+			if (callback ) callback(id)
+		});
 	};
 
-	this.list = function (req, res, next) {
+	this.list = function (req, res, callback) {
 		var appid = Number(req.params.appid);
 		var collection = prefix + col;
 		converter.toIDs(['_appid', '_isDraft'], function (ids) {
@@ -45,81 +47,74 @@ exports.CRUD = function (db, mongodb, async, converter, prefix, col) {
 			var cursor = db.collection(collection).find(query, projection);
 			var done = false;
 			var results = [];
-			async.whilst(
-					function () {
-						return done == false;
-					},
-					function (callback) {
-						cursor.next().then(function (r) {
-							if (r) {
-								converter.toOriginal(r).then(function (r) {
-									results.push(r);
-									callback(null);
-								}).catch(function (err) {
-									callback(err);
-								});
-							} else {
-								done = true;
-								callback(null, results);
-							}
-						}).catch(function (err) {
-							callback(err);
+			async.whilst(function () {
+				return done == false;
+			}, function (callback) {
+				cursor.next(function (err, r) {
+					if (err) throw err;
+					if (r) {
+						converter.toOriginal(r, function (r) {
+							results.push(r);
+							callback(null);
 						});
-					}, function (err, results) {
-						if (err) {
-							console.log('haha');
-							next(err);
-						} else {
-							res.json(results);
-						}
-					});
-
-		});
-	}
-
-	this.match = function (req, res, next) {
-		// var appid = req.params.appid;
-		var atid = req.params.id;
-		var collection = prefix + col;
-		converter.toID('_appid')
-				.then(function (r) {
-					var query = {
-						_id: new mongodb.ObjectID(atid)
-					};
-					var projection = {};
-					projection[r] = 0;
-					return db.collection(collection).find(query, projection).toArray();
-				}).then(function (r) {
-					if (r.length != 0) {
-						converter.toOriginal(r[0]).then(function (r) {
-							res.json(r);
-						}).catch(next);
 					} else {
-						res.status(200).end();
+						done = true;
+						callback(null, results);
 					}
-				}).catch(next);
+				});
+			}, function (err, results) {
+				if (err) throw err;
+				res.json(results);
+				if (callback ) callback(results);
+			});
+		});
 	};
 
-	this.delete = function (req, res, next) {
+	this.match = function (req, res, callback) {
 		// var appid = req.params.appid;
 		var atid = req.params.id;
 		var collection = prefix + col;
-		db.collection(collection).deleteOne({_id: new mongodb.ObjectID(atid)})
-			.then(function (r) {
-				res.status(200).end();
-			}).catch(next);
+		converter.toID('_appid', function (r) {
+			var query = {_id: new mongodb.ObjectID(atid)};
+			var projection = {};
+			projection[r] = 0;
+			db.collection(collection).find(query, projection).toArray(function (err, r) {
+				if (err) throw err;
+
+				if (r.length != 0) {
+					converter.toOriginal(r[0], function (r) {
+						res.json(r);
+						if(callback) callback();
+					});
+				} else {
+					res.status(200).end();
+				}
+			});
+		});
+	};
+
+	this.delete = function (req, res, callback) {
+		// var appid = req.params.appid;
+		var atid = req.params.id;
+		var collection = prefix + col;
+		db.collection(collection).deleteOne({_id: new mongodb.ObjectID(atid)}, function (err, r) {
+			if (err) throw err;
+			res.status(200).end();
+			if (callback !== undefined) callback();
+		});
 	};
 
 	this.update = function (req, res, next) {
 		var data = req.body;
 		var atid = req.params.id;
 		var collection = prefix + col;
-		converter.toObject(data)
-				.then(function (r) {
-					return db.collection(collection).updateOne({_id: new mongodb.ObjectID(atid)}, {$set: r})
-				}).then(function (results) {
-			res.status(200).end();
-		}).catch(next);
+		converter.toObject(data, function (r) {
+			db.collection(collection).updateOne({_id: new mongodb.ObjectID(atid)}, {$set: r}, function (err, results) {
+				if (err) throw  err;
+				res.status(200).end();
+				if (callback !== undefined) callback();
+			});
+		});
 	};
 
 	this.deleteDraf = function (req, res) {
@@ -129,9 +124,11 @@ exports.CRUD = function (db, mongodb, async, converter, prefix, col) {
 			var query = {};
 			query[ids['_appid']] = appid;
 			query[ids['_isDraft']] = true;
-			db.collection(collection).deleteMany(query).then(function (r) {
+			db.collection(collection).deleteMany(query, function (err, r) {
+				if (err) throw err;
 				res.status(200).end();
-			}).catch(next);
+				if (callback) callback();
+			});
 		});
 	}
 };
