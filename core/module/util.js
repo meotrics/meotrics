528c39def209a99508d88d648b1e3288b76d72ea
Administrator <thanhpk@live.com>
2016-04-14 13:18:54 +0700

    remove promise and stardardlize constructing parameters

--- core/module/util.js
+++ core/module/util.js
@@ -1,76 +1,68 @@
-var config = require('config');
-var async = require('async');
-var mongodb = require('mongodb');
-var prefix = config.get("mongod.prefix");
-
-var Util = function(db, converter){
+var Util = function (db, mongodb,  async, converter, prefix) {
 	this.db = db;
 	this.converter = converter;
-}
+	this.async = async;
+	this.prefix = prefix;
+};
 
 module.exports = Util;
 
-Util.prototype.updateUserInf = function(appid, _mtid, data){
+Util.prototype.updateUserInf = function (appid, _mtid, data) {
 	console.log(data);
-	var collection = prefix + appid;
+	var _self = this;
+	var collection = _self.prefix + appid;
 
 	var keys = Object.keys(data);
-	var _self = this;
 
-	async.each(keys, function(key, callback){
+	_self.async.each(keys, function (key, callback) {
 		var fieldInDb;
 		var query;
 
-		_self.converter.toID(key).then(function(r){
+		_self.converter.toID(key, function (r) {
 			fieldInDb = r;
 			query = {_id: new mongodb.ObjectID(_mtid)};
 			var projection = {};
 			projection[fieldInDb] = 1;
-			return _self.db.collection(collection).find(query, projection).toArray();
-		}).then(function(r){
-			if(r.length == 0){
-				callback(null);
-			}else{
-				var record = r[0];
-				var temp = [];
-
-				if(record[fieldInDb] != undefined){
-					temp = record[fieldInDb];
-				}
-				var value = data[key];
-				var length = temp.length;
-				for(var i=0;i<length;i++){
-					if(temp[i] == value){
-						break;
-					}
-				}
-
-				if(i == length){
-					temp.push(value);
-					temp.sort();
-					var update = {};
-					update[fieldInDb] = temp;
-
-					_self.db.collection(collection).updateOne(query, {"$set": update}).then(function(r){
-						callback(null);
-					}).catch(function(e){
-						callback(e);
-					});
-				}else{
+			_self.db.collection(collection).find(query, projection).toArray(function (err, r) {
+				if (err) throw err;
+				if (r.length == 0) {
 					callback(null);
-				}
+				} else {
+					var record = r[0];
+					var temp = [];
 
-			}
-		}).catch(function(e){
-			callback(e);
+					if (record[fieldInDb] != undefined) {
+						temp = record[fieldInDb];
+					}
+					var value = data[key];
+					var length = temp.length;
+					for (var i = 0; i < length; i++) {
+						if (temp[i] == value) {
+							break;
+						}
+					}
+
+					if (i == length) {
+						temp.push(value);
+						temp.sort();
+						var update = {};
+						update[fieldInDb] = temp;
+
+						_self.db.collection(collection).updateOne(query, {"$set": update}, function (err, r) {
+							if (err) throw err;
+
+							callback(null);
+						});
+					} else {
+						callback(null);
+					}
+
+				}
+			});
 		});
-	}, function(err){
-		if(err){
-			// TODO:
-			console.log(err);
-		}else{
-			// TODO:
-		}
+	}, function (err) {
+		if (err) throw err;
+
 	});
 };
 
