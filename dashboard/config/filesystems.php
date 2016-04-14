17c00b78ed166eef532c609f9394d1aa87e02aaa
Administrator <thanhpk@live.com>
2016-04-14 17:49:51 +0700

    add refer to public code

--- dashboard/config/filesystems.php
+++ dashboard/config/filesystems.php
@@ -43,6 +43,11 @@ return [
 
 	'disks' => [
 
+		'resouce' => [
+			'driver' => 'local',
+			'root' => public_path(),
+		],
+
 		'local' => [
 			'driver' => 'local',
 			'root'   => storage_path().'/app',
