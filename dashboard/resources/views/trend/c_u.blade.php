ec6ef8a47358ae15822b1fdb68e638ced743ceb1
thanhpk <thanhpk@live.com>
2016-04-19 03:33:29 +0700

    refine trend ui

--- dashboard/resources/views/trend/c_u.blade.php
+++ dashboard/resources/views/trend/c_u.blade.php
@@ -5,151 +5,151 @@
 
 @section('content')
 
-<div class="card row">
-    <div class="header col-md-12">
-        <form class="form-horizontal" method="post" action="{{URL::to('trend/write')}}">
-            <input type="hidden" name="Trend[_id]" value="{{$trend->_id}}"/>
-            <div class="form-group col-md-12">
-                <label class="col-md-2" style="margin-top: 10px">Trend name</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control " name="Trend[name]" required="" value="{{$trend->name}}"/>
-                    @if($errors->any())
-                    <p class="errror">{{$errors->first('name')}}</p>
-                    @endif
-                </div>
-            </div>
+	<div class="card row">
+		<div class="header col-md-12">
+			<form class="form-horizontal" method="post" action="{{URL::to('trend/write')}}">
+				<input type="hidden" name="Trend[_id]" value="{{$trend->_id}}"/>
+				<div class="form-group col-md-12">
+					<label class="col-md-2" style="margin-top: 10px">Trend name</label>
+					<div class="col-md-4">
+						<input type="text" class="form-control " name="Trend[name]" required="" value="{{$trend->name}}"/>
+						@if($errors->any())
+							<p class="errror">{{$errors->first('name')}}</p>
+						@endif
+					</div>
+				</div>
 
-            <div class="form-group col-md-12">
-                <label class="col-md-2" style="margin-top: 10px">List top</label>
-                <div class="col-md-4">
-                    <select class="form-control" id="typeid" name="Trend[typeid]">
-                        @if($actiontypes)
-                            $objects = [];
-                            @foreach($actiontypes as $actiontype)
-                                @if($actiontype->codename && $actiontype->name)
-                                    $objects[$actiontype->codename] = $actiontype->fields;
-                                    <option value="{{$actiontype->codename}}" 
-                                            {{$trend->typeid == $actiontype->codename ? 'selected=""' : ''}}>
-                                        {{$actiontype->name}}
-                                    </option>
-                                @endif
-                            @endforeach
-                        @endif
-                    </select>
-                </div>
-                <div class="col-md-4">
-                    <select class="form-control" id="object" name="Trend[object]">
-                        <option value="">Object list</option>
-                    </select>
-                </div>
-            </div>
+				<div class="form-group col-md-12">
+					<label class="col-md-2" style="margin-top: 10px">List top</label>
+					<div class="col-md-4">
+						<select class="form-control" id="typeid" name="Trend[typeid]">
+							@if($actiontypes)
+								$objects = [];
+								@foreach($actiontypes as $actiontype)
+									@if($actiontype->codename && $actiontype->name)
+										$objects[$actiontype->codename] = $actiontype->fields;
+										<option value="{{$actiontype->codename}}"
+														{{$trend->typeid == $actiontype->codename ? 'selected=""' : ''}}>
+											{{$actiontype->name}}
+										</option>
+									@endif
+								@endforeach
+							@endif
+						</select>
+					</div>
+					<div class="col-md-4">
+						<select class="form-control" id="object" name="Trend[object]">
+							<option value="">Object list</option>
+						</select>
+					</div>
+				</div>
 
-            <div class="form-group col-md-12">
-                <label class="col-md-2" style="margin-top: 10px">Which has</label>
-                <div class="col-md-4">
-                    <select class="form-control" id="meotrics" name="Trend[meotrics]">
-                        <option value="">Meotrics list</option>
-                    </select>
-                    <input type="hidden" id="operation" name="Trend[operation]" value="{{$trend->operation}}"/>
-                    <input type="hidden" id="param" name="Trend[param]" value="{{$trend->param}}"/>
-                </div>
-            </div>
+				<div class="form-group col-md-12">
+					<label class="col-md-2" style="margin-top: 10px">Which has</label>
+					<div class="col-md-4">
+						<select class="form-control" id="meotrics" name="Trend[meotrics]">
+							<option value="">Meotrics list</option>
+						</select>
+						<input type="hidden" id="operation" name="Trend[operation]" value="{{$trend->operation}}"/>
+						<input type="hidden" id="param" name="Trend[param]" value="{{$trend->param}}"/>
+					</div>
+				</div>
 
-            <div class="form-group col-md-12">
-                <label class="col-md-2" style="margin-top: 10px">Order</label>
-                <div class="col-md-4">
-                    <select class="form-control" id="order" name="Trend[order]">
-                        <option value="1" {{$trend->order == 1 ? 'selected=""' : ''}}>ASC</option>
-                        <option value="-1" {{$trend->order == -1 ? 'selected=""' : ''}}>DESC</option>
-                    </select>
-                </div>
-            </div>
+				<div class="form-group col-md-12">
+					<label class="col-md-2" style="margin-top: 10px">Order</label>
+					<div class="col-md-4">
+						<select class="form-control" id="order" name="Trend[order]">
+							<option value="1" {{$trend->order == 1 ? 'selected=""' : ''}}>ASC</option>
+							<option value="-1" {{$trend->order == -1 ? 'selected=""' : ''}}>DESC</option>
+						</select>
+					</div>
+				</div>
 
-            <div class="row">
-                <div class="col-sm-push-2 col-sm-2">
-                    <button type="submit" class="btn btn-success btn-fill ">
-                        <i class="pe-7s-diskette mr5" style="font-size:19px; vertical-align: middle"></i>
-                        <span class="" style="vertical-align: middle">{{$trend->_id ? 'Update' : 'Create'}}</span>
-                    </button>
-                </div>
+				<div class="row">
+					<div class="col-sm-push-2 col-sm-2">
+						<button type="submit" class="action button blue">
 
-            </div>
-        </form>
-    </div>
+							<span class="label">{{$trend->_id ? 'Update' : 'Create'}}</span>
+						</button>
+					</div>
 
-</div>
+				</div>
+			</form>
+		</div>
+
+	</div>
 
 
 @endsection
 
 @section('additional')
-<script type="text/javascript">
-var objects = {};
-@if($actiontypes)
-    @foreach($actiontypes as $actiontype)
-        objects['{{$actiontype->codename}}'] = [];
-        @foreach($actiontype->fields as $field)
-            objects['{{$actiontype->codename}}'].push({
-                'pcode': '{{$field->pcode}}',
-                'pname': '{{$field->pname}}',
-            });
-        @endforeach
-    @endforeach
+	<script type="text/javascript">
+		var objects = {};
+		@if($actiontypes)
+						@foreach($actiontypes as $actiontype)
+						objects['{{$actiontype->codename}}'] = [];
+		@foreach($actiontype->fields as $field)
+						objects['{{$actiontype->codename}}'].push({
+			'pcode': '{{$field->pcode}}',
+			'pname': '{{$field->pname}}',
+		});
+		@endforeach
+@endforeach
 @endif
 actionChange($('#typeid').val());
-$('#typeid').on('change', function(){
-    actionChange($(this).val());
-});
+		$('#typeid').on('change', function () {
+			actionChange($(this).val());
+		});
 
-function actionChange(typeid){
-    $('#object').html('');
-    if(objects[typeid] && objects[typeid].length){
-        $('#object').show();
-        $.each(objects[typeid], function(i, v){
-            $('#object').append('<option value="'+v.pcode+'">'+v.pname+'</option>');
-        });
-    }
-    else{
-        $('#object').hide();
-    }
-    
-    $.ajax({
-        type: 'GET',
-        dataType: 'JSON',
-        url: '{{ URL::to('trend/meotrics', ['app_id' => $app_id]) }}' + '/action_id/' +  typeid,
-        success: function(data){
-            if(data.success && data.meotrics){
-                $('#meotrics').html('');
-                var selected_value = '';
-                var selected_key = ''
-                $.each(data.meotrics, function(i, v){
-                    if($('#operation').val() == v.operation && $('#param').val() == v.param){
-                        selected_value = v;
-                        selected_key = i;
-                    }
-                    $('#meotrics').append('<option value="'+i+'" \n\
-                        data-operation="'+v.operation+'"\n\
-                        data-param="'+v.param+'">'+v.name+'</option>');
-                });
-                if(!selected_value){
-                    selected_value = data.meotrics[0];
-                    selected_key = 0;
-                }
-                $('#meotrics').val(selected_key);
-                meotricsChange(selected_value.operation, selected_value.param);
-            }
-        },
-    });
-    return false;
-}
+		function actionChange(typeid) {
+			$('#object').html('');
+			if (objects[typeid] && objects[typeid].length) {
+				$('#object').show();
+				$.each(objects[typeid], function (i, v) {
+					$('#object').append('<option value="' + v.pcode + '">' + v.pname + '</option>');
+				});
+			}
+			else {
+				$('#object').hide();
+			}
 
-$('#meotrics').on('change', function(){
-    meotricsChange($(this).attr('data-operation'), $(this).attr('data-param'));
-});
+			$.ajax({
+				type: 'GET',
+				dataType: 'JSON',
+				url: '{{ URL::to('trend/meotrics', ['app_id' => $app_id]) }}' + '/action_id/' + typeid,
+				success: function (data) {
+					if (data.success && data.meotrics) {
+						$('#meotrics').html('');
+						var selected_value = '';
+						var selected_key = ''
+						$.each(data.meotrics, function (i, v) {
+							if ($('#operation').val() == v.operation && $('#param').val() == v.param) {
+								selected_value = v;
+								selected_key = i;
+							}
+							$('#meotrics').append('<option value="' + i + '" \n\
+                        data-operation="' + v.operation + '"\n\
+                        data-param="' + v.param + '">' + v.name + '</option>');
+						});
+						if (!selected_value) {
+							selected_value = data.meotrics[0];
+							selected_key = 0;
+						}
+						$('#meotrics').val(selected_key);
+						meotricsChange(selected_value.operation, selected_value.param);
+					}
+				},
+			});
+			return false;
+		}
 
-function meotricsChange(operation, param){
-    $('#operation').val(operation);
-    $('#param').val(param);
-}
-</script>
+		$('#meotrics').on('change', function () {
+			meotricsChange($(this).attr('data-operation'), $(this).attr('data-param'));
+		});
+
+		function meotricsChange(operation, param) {
+			$('#operation').val(operation);
+			$('#param').val(param);
+		}
+	</script>
 @endsection
--- dashboard/resources/views/trend/index.blade.php
+++ dashboard/resources/views/trend/index.blade.php
@@ -1,5 +1,4 @@
 <?php
-use App\Enum\TrendEnum;
 ?>
 @extends('../layout/master', ['sidebarselect' => 'trend'])
 @section('title', 'Trend')
@@ -9,7 +8,7 @@ each $types have fields in it
 
 --}}
 @section('script')
-	
+
 @endsection
 
 @section('style')
@@ -23,37 +22,37 @@ each $types have fields in it
 			<form class="">
 				<label class="">Select trend</label>&nbsp;&nbsp;
 				<select id="trend" class="form-control input-sm " style="width: 250px; display:inline-block">
-				    <?php
-                                    $trend_first = null;
-                                    if($trends):    
-                                        $trend_first = $trends[0];
-                                    ?>
-                                        @foreach($trends as $trend)
-                                        <option value="{{$trend->_id}}">{{$trend->name ? $trend->name : TrendEnum::EMPTY_NAME}}</option>
-                                        @endforeach
-                                    <?php
-                                    endif;
-                                    ?>
+					<?php
+					$trend_first = null;
+					if($trends):
+					$trend_first = $trends[0];
+					?>
+					@foreach($trends as $trend)
+						<option value="{{$trend->_id}}">{{$trend->name ? $trend->name : TrendEnum::EMPTY_NAME}}</option>
+					@endforeach
+					<?php
+					endif;
+					?>
 				</select>
-                                <a id="action_update" data-href="{{URL::to('trend/update')}}" href="{{URL::to('trend/update', [
+				<a id="action_update" data-href="{{URL::to('trend/update')}}" href="{{URL::to('trend/update', [
                                     'id' => $trend_first ? $trend_first->_id : ''
                                 ])}}" class="button action blue" role="button"><span class="label">Update</span></a>
-                                <a id="action_delete" href="javascript:void(0)" class="btn btn-danger" role="button">Delete</a>
+				<a id="action_delete" href="javascript:void(0)" class="button action red" role="button"><span class="label">Delete</span></a>
 				&nbsp; or &nbsp;<a href="{{ URL::to('trend/create') }}">+ CREATE NEW TREND</a>
 			</form>
 		</div>
 
 
-                <div class="content col-md-12" id="outputs_table">
-                    <?php
-                    if($outputs):
-                    ?>
-                    @include('trend.partials.outputs', [
-                        'outputs' => $outputs,
-                    ])
-                    <?php
-                    endif;
-                    ?>
+		<div class="content col-md-12" id="outputs_table">
+			<?php
+			if($outputs):
+			?>
+			@include('trend.partials.outputs', [
+					'outputs' => $outputs,
+			])
+			<?php
+			endif;
+			?>
 		</div>
 	</div>
 
@@ -61,43 +60,43 @@ each $types have fields in it
 @endsection
 
 @section('additional')
-<script src="{{asset('js/select2.min.js')}}"></script>
-<script type="text/javascript">
-    $('select').select2();
-    
-    $('#trend').on('change', function(){
-        var that = $(this);
-        $('#action_update').attr('href', $('#action_update').attr('data-href')+'/'+that.val());
-        $.ajax({
-            type: 'GET',
-            dataType: 'JSON',
-            url: '{{ URL::to('trend/htmloutputs') }}',
-            data: {
-                '_id' : that.val(),
-            },
-            success: function(data){
-                if(data.success && data.html_outputs){
-                    $('#outputs_table').html(data.html_outputs);
-                }
-            },
-        });
-    return false;
-    });
-    
-    $('#action_delete').on('click', function(){
-        var cf = confirm('This trend will be removed. Are you sure?');
-        if(cf){
-            $.ajax({
-            type: 'DELETE',
-            dataType: 'JSON',
-            url: '{{ URL::to('trend/remove') }}'+'/'+$('#trend').val(),
-            success: function(data){
-                if(data.success){
-                    location.reload();
-                }
-            },
-        });
-        }
-    });
-</script>    
+	<script src="{{asset('js/select2.min.js')}}"></script>
+	<script type="text/javascript">
+		$('select').select2();
+
+		$('#trend').on('change', function () {
+			var that = $(this);
+			$('#action_update').attr('href', $('#action_update').attr('data-href') + '/' + that.val());
+			$.ajax({
+				type: 'GET',
+				dataType: 'JSON',
+				url: '{{ URL::to('trend/htmloutputs') }}',
+				data: {
+					'_id': that.val(),
+				},
+				success: function (data) {
+					if (data.success && data.html_outputs) {
+						$('#outputs_table').html(data.html_outputs);
+					}
+				},
+			});
+			return false;
+		});
+
+		$('#action_delete').on('click', function () {
+			var cf = confirm('This trend will be removed. Are you sure?');
+			if (cf) {
+				$.ajax({
+					type: 'DELETE',
+					dataType: 'JSON',
+					url: '{{ URL::to('trend/remove') }}' + '/' + $('#trend').val(),
+					success: function (data) {
+						if (data.success) {
+							location.reload();
+						}
+					},
+				});
+			}
+		});
+	</script>
 @endsection
