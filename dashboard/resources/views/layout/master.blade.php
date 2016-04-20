a5ae0a2795df36c97a9205314050671eddac2f6e
Administrator <thanhpk@live.com>
2016-04-20 15:35:50 +0700

    remove footer

--- dashboard/resources/views/layout/master.blade.php
+++ dashboard/resources/views/layout/master.blade.php
@@ -255,40 +255,6 @@
 				@yield('content')
 			</div>
 		</div>
-
-
-		<footer class="footer">
-			<div class="container-fluid">
-				<nav class="pull-left">
-					<ul>
-						<li>
-							<a href="#">
-								Home
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								Company
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								Portfolio
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								Blog
-							</a>
-						</li>
-					</ul>
-				</nav>
-				<p class="copyright pull-right">
-					&copy; 2016 <a href="{{ URL::to('/') }}">Meotrics</a>,
-					made with <big class="text-danger">&hearts;</big> for a better analyze
-				</p>
-			</div>
-		</footer>
 	</div>
 </div>
 
