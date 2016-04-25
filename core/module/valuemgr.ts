98ef9ab6d96f32f2e93bca0dc6ec2ed2d41cc1ab
Administrator <thanhpk@live.com>
2016-04-25 14:17:13 +0700

    add valuemgr

---
+++ core/module/valuemgr.ts
@@ -0,0 +1,79 @@
+import * as redis from 'redis';
+//import * as redisscanner from './redisscanner';
+import rscn = require('../utils/redisscanner');
+
+export class ValueMgr {
+	private redisscanner: rscn.RedisScanner;
+
+	public constructor(private rdb: redis.RedisClient) {
+
+		this.redisscanner = new rscn.RedisScanner(rdb);
+	}
+
+	public convert(appid: string, object: any, fields: string[], callback: () => any) {
+		var c = 0;
+		for (var f of fields)
+			this.cineValue(appid, f, object[f], function (number) {
+				c++;
+				if (c == fields.length)
+					return callback;
+				object[f] = number;
+			});
+	}
+
+	public cineValue(appid: string, field: string, value: string, callback?: (number) => any) {
+		var me = this;
+		var prop = this.queryToPattern(appid, field, 'eq', value);
+		this.rdb.get(prop, function (err, ret) {
+			if (ret != null) {
+				if (callback) return callback(parseInt(ret));
+				return;
+			}
+			else {
+				var pattern = this.queryToPattern(appid, field) + "*";
+				this.redisscanner.count(pattern, function (ret) {
+					// must be synchonyze
+					me.rdb.set(this.queryToPattern(appid, field) + ":" + value, ret + 1);
+					if (callback) callback(ret + 1);
+					return;
+				})
+			}
+		});
+	}
+
+	private queryToPattern(appid: string, field: string, operator?: string, value?: string): string {
+		var pattern = "field" + ":" + appid + ":" + field + ":";
+		switch (operator) {
+			case 'eq':
+				pattern += value;
+				break;
+			case 'con':
+				pattern += "*" + value + "*";
+			case 'ncon':
+				pattern += value;
+			case 'sw':
+				pattern += "*" + value;
+			case 'ew':
+				pattern += value + "*";
+			default:
+				break;
+		}
+		return pattern;
+	}
+
+	public convertCondition(appid: string, field: string, operator: string, value: string, callback: (string) => void) {
+		var me = this;
+		var pattern = this.queryToPattern(appid, field, operator, value);
+		this.redisscanner.scan(pattern, function (keys) {
+			me.rdb.mget(keys, function (err, values: string[]) {
+				if (err) throw err;
+				var condition = {};
+				if (condition == 'ncon' || condition == 'ne')
+					condition[field] = { $nin: values };
+				else
+					condition[field] = { $in: values };
+				callback(condition);
+			});
+		});
+	}
+}
