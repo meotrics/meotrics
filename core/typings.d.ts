595efc6777bb91c27e189e7d492430fa22bfb54d
Administrator <thanhpk@live.com>
2016-04-25 15:07:40 +0700

    remove duplicate es6 collection support

--- core/typings.d.ts
+++ core/typings.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="../../DefinitelyTyped/redis/redis.d.ts" />
 /// <reference path="../../DefinitelyTyped/node/node.d.ts" />
 /// <reference path="./utils/redisscanner.ts" />
-/// <reference path="../../DefinitelyTyped/es6-collections/es6-collections.d.ts" />
