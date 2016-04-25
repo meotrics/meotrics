2b4a4eb18242da1ffaffa3d6a3420b9bec980a02
Administrator <thanhpk@live.com>
2016-04-25 14:16:38 +0700

    change to typescript

---
+++ core/typings.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../DefinitelyTyped/redis/redis.d.ts" />
+/// <reference path="../../DefinitelyTyped/node/node.d.ts" />
+/// <reference path="./utils/redisscanner.ts" />
+/// <reference path="../../DefinitelyTyped/es6-collections/es6-collections.d.ts" />
