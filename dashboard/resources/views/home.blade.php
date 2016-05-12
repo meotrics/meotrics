082ffeefd8d2f1b4bbea83a1c6dd9049b3435eab
Pham Kieu Thanh <thanhpk@live.com>
2016-05-12 13:33:27 +0700

    add charts

--- dashboard/resources/views/home.blade.php
+++ dashboard/resources/views/home.blade.php
@@ -4,6 +4,115 @@
 
 @endsection
 @section('script')
+<script src="{{asset('/js/Chart.js')}}"></script>
+<script>
+var options = {
+    legend: {display:false},
+    segmentShowStroke: false,
+    animateRotate: false,
+    animation : false,
+    percentageInnerCutout: 50,
+    scaleShowLabels: false,
+    tooltipTemplate: "<%= svalue %>%",
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].lineColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+}
+
+var trChart = new Chart($("#trchart"), {
+    type: 'line',
+    data: {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+        }
+    ]
+},
+    options: {
+        xAxes: [{
+            display: false
+        }]
+    }
+});
+
+var trChart = new Chart($("#trchartpc"), {
+    type: 'line',
+    data: {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+        }
+    ]
+},
+    options: {
+        xAxes: [{
+            display: false
+        }]
+    }
+});
+
+var visitChart = new Chart($("#visitchart"), {
+    type: 'doughnut',
+    data: {
+    labels: [
+        "<span class='dot'></span> New visitor",
+       "<span class='dot'></span> Returning visitor",
+    ],
+    datasets: [
+        {
+            data: [300, 50, 100],
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }]}, options: options
+    
+});
+
+$('#visitchartlegend').html( visitChart.generateLegend());
+</script>
 @endsection
 
 @section('content')
@@ -132,6 +241,60 @@
             </div>
         </div>
     </div>
+
+<div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="header">
+                                Visits
+                                <p class="category pull-right">TODAY</p>    
+                            </div>                        
+                            <div class="content">
+                              <div class="row">
+                                  <div class="col-sm-6">
+                                      430 <br/>
+                                      23
+                                  </div>
+                                  <div class="col-sm-6">
+                                      <canvas id="visitchart" width="400" height="400" ></canvas>
+
+                                      <div id="visitchartlegend" class="chart-legend"></div>
+                                  </div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="header">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        This week
+                                    </div>
+                                    <div class="col-sm-6">
+                                        This week
+                                    </div>
+                                </div>
+                            </div>                        
+                            <div class="content">
+                                 <div class="row">
+                                    <div class="col-sm-6">
+                                        <canvas id="trchart" width="400" height="400"> </canvas>
+                                        Total revenue
+                                        $ 2 238
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <canvas id="trchartpc" width="400" height="400"> </canvas>
+                                        Revenue per customer
+                                        $ 13.5
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
 @endsection
 
 
