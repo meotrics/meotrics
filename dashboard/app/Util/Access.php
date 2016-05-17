d8574ef4290dc546c3a45179fcfad86ba549c3b2
Pham Kieu Thanh <thanhpk@live.com>
2016-05-17 21:41:06 +0700

    add ability for app owner

--- dashboard/app/Util/Access.php
+++ dashboard/app/Util/Access.php
@@ -6,7 +6,8 @@ namespace App\Util;
 class Access
 {
 
-	public static function getPerm($appid){
+	public static function getPerm($appid)
+	{
 
 	}
 
@@ -19,18 +20,33 @@ class Access
 	// return 0 if sucecss
 	// -1: access deny
 	// -2 other iser not exist in app, must add first
+	// -3 appid doesn't exist
+	// -4 cannot set perm for owner
 	public static function setPerm($userid, $otheruser, $appid, $can_perm, $can_struct, $can_report)
 	{
+		// get owner
+		$app = DB::table('apps')->where('id', $appid)->last();
+		if ($app == null) return -3;
+
 		if (self::can_editPerm($userid, $appid)) {
-			
+
 			$perm = DB::table('user_app')->where('appid', $appid)->where('userid', $otheruser)->last();
 			if ($perm == null) {
-				DB::table('user_app')->insert(
-					['appid' => $appid, 'userid' => $otheruser, 'can_perm' => 0, 'can_struct' => 0, 'can_report' => 1]
-				);
-
+				if ($app->ownerid == $otheruser)
+					DB::table('user_app')->insert(
+						['appid' => $appid, 'userid' => $otheruser, 'can_perm' => 1, 'can_struct' => 1, 'can_report' => 1]
+					);
+				else
+					DB::table('user_app')->insert(
+						['appid' => $appid, 'userid' => $otheruser, 'can_perm' => 0, 'can_struct' => 0, 'can_report' => 1]
+					);
+			} else {
+				if ($app->ownerid == $otheruser){
+					return -4;
+				}
 			}
-		 $permrecord = [];
+
+			$permrecord = [];
 			if ($can_perm != null) $permrecord['can_perm'] = $can_perm;
 			if ($can_struct != null) $permrecord['can_struct'] = $can_struct;
 			if ($can_report != null) $permrecord['can_report'] = $can_report;
@@ -42,6 +58,11 @@ class Access
 
 	public static function can_editPerm($userid, $appid)
 	{
+		// full access for app owner
+		$app = DB::table('apps')->where('id', $appid)->last();
+		if ($app == null) return -3;
+		if ($app->ownerid == $userid) return true;
+
 		$perm = DB::table('apps')->where('appid', $appid)->where('userid', $userid)->last();
 		if ($perm == null) return false;
 		if ($perm->can_perm == 1) return true;
@@ -50,6 +71,11 @@ class Access
 
 	public static function can_editStruct($userid, $appid)
 	{
+		// full access for app owner
+		$app = DB::table('apps')->where('id', $appid)->last();
+		if ($app == null) return -3;
+		if ($app->ownerid == $userid) return true;
+
 		$perm = DB::table('apps')->where('appid', $appid)->where('userid', $userid)->last();
 		if ($perm == null) return false;
 		if ($perm->can_struct == 1) return true;
@@ -58,6 +84,11 @@ class Access
 
 	public static function can_editReport($userid, $appid)
 	{
+		// full access for app owner
+		$app = DB::table('apps')->where('id', $appid)->last();
+		if ($app == null) return -3;
+		if ($app->ownerid == $userid) return true;
+
 		$perm = DB::table('apps')->where('appid', $appid)->where('userid', $userid)->last();
 		if ($perm == null) return false;
 		if ($perm->can_report == 1) return true;
