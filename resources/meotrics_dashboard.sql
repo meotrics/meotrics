89b63c16b027a29d45ae50ee08124b1ed954e7b1
Pham Kieu Thanh <thanhpk@live.com>
2016-05-19 13:49:43 +0700

    change database for multiple user

--- resources/meotrics_dashboard.sql
+++ resources/meotrics_dashboard.sql
@@ -1,19 +1,26 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : uniserverz
-Source Server Version : 50536
-Source Host           : localhost:3306
-Source Database       : meotrics_dashboard
-
-Target Server Type    : MYSQL
-Target Server Version : 50536
-File Encoding         : 65001
-
-Date: 2016-03-20 21:39:17
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `apps`
+-- ----------------------------
+DROP TABLE IF EXISTS `apps`;
+CREATE TABLE `apps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `ownerid` int(11) NOT NULL,
+  `code` varchar(30) NOT NULL,
+  `backendid` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `codename` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of apps
+-- ----------------------------
+INSERT INTO `apps` VALUES ('1', 'Xa lo van su', '1', 'XALOVANSU', '');
+INSERT INTO `apps` VALUES ('2', 'Speedy Apply', '1', 'SPEEDYAPPLY', '');
+INSERT INTO `apps` VALUES ('3', 'Speedy Access', '1', 'SPEEDYACCESS', '');
+INSERT INTO `apps` VALUES ('4', 'Okr', '1', 'OKR', '');
 
 -- ----------------------------
 -- Table structure for `users`
@@ -37,9 +44,30 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `password_reset_token` (`password_reset_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('1', '', '$2y$10$vlR4xsKws521k0JDWkW73ezOHBXDGZePi048qkiGfCklW/KHsD9KK', null, 'thanhpk@live.com', null, null, null, null, '10', '2016', '2016', 'Thanh', 'd6BARrAe7dEurIKT97ftvlqhFh1grqhnyRcWtc7TmK2VC68z0QucJ7Zeamjp');
+INSERT INTO `users` VALUES ('1', '', '$2y$10$vlR4xsKws521k0JDWkW73ezOHBXDGZePi048qkiGfCklW/KHsD9KK', null, 'thanh@gmail.com', null, null, null, null, '10', '2016', '2016', 'Thanh', 'z9gMXGYyEvvvYQUYNbHCFohFyz3530gQJJbcQEJkHtvBN0idGRhxScSpJS7l');
+INSERT INTO `users` VALUES ('2', '', '$2y$10$xaERgAC4H.p5BLE1efhoWeDmzTNyFaS4D0pKbhmHET1AwA31sIYF2', null, 'van@gmail.com', null, null, null, null, '10', '2016', '2016', 'Hai van', null);
+
+-- ----------------------------
+-- Table structure for `user_app`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_app`;
+CREATE TABLE `user_app` (
+  `appid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `can_perm` tinyint(1) NOT NULL,
+  `can_struct` tinyint(1) NOT NULL,
+  `can_report` tinyint(1) NOT NULL,
+  PRIMARY KEY (`appid`,`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_app
+-- ----------------------------
+INSERT INTO `user_app` VALUES ('1', '1', '1', '1', '1');
+INSERT INTO `user_app` VALUES ('4', '1', '1', '1', '1');
+INSERT INTO `user_app` VALUES ('4', '2', '0', '1', '1');
