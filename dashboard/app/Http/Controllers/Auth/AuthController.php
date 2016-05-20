0ff4a4c00ebb49cc8cac1ab722228d5722e44c4e
Pham Kieu Thanh <thanhpk@live.com>
2016-05-20 17:47:29 +0700

    login with google backend

--- dashboard/app/Http/Controllers/Auth/AuthController.php
+++ dashboard/app/Http/Controllers/Auth/AuthController.php
@@ -5,7 +5,8 @@ use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
-class AuthController extends Controller {
+class AuthController extends Controller
+{
 
 	/*
 	|--------------------------------------------------------------------------
@@ -23,8 +24,8 @@ class AuthController extends Controller {
 	/**
 	 * Create a new authentication controller instance.
 	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+	 * @param  \Illuminate\Contracts\Auth\Guard $auth
+	 * @param  \Illuminate\Contracts\Auth\Registrar $registrar
 	 * @return void
 	 */
 	public function __construct(Guard $auth, Registrar $registrar)
@@ -33,6 +34,46 @@ class AuthController extends Controller {
 		$this->registrar = $registrar;
 
 		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+
+	public function googlelogin(Request $request)
+	{
+		$tokenid = $request->input('tokenid');
+
+
+		$url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" . $tokenid;
+
+		$options = array('http' => array(
+			'header' => "Content-type: application/json\r\n",
+			'method' => 'GET'
+		));
+		$context = stream_context_create($options);
+
+		$output = file_get_contents($url, false, $context);
+		if (substr_compare($http_response_header[0], "200 OK", strlen($http_response_header[0]) - strlen("200 OK"), strlen("200 OK")) == 0) {
+			$data = MtHttp::json_decodeEx($output);
+			$user = DB::table('users')->where('email', $data->email)->first();
+			if ($user == null) {
+				// send a confirm mail
+				$user->id = DB::table('users')->insertGetId(['password' => '',
+					'email' => $data->email,
+					'name' => $data->name,
+					'status' => 10,
+					'created_at' => date("Y"),
+					'updated_at' => date("Y"),
+					'verified' => 1]);
+			} else {
+				// update verified token
+				DB::table('users')->where('email', $data->email)->update('verified', 1);
+
+			}
+
+			\Auth::loginUsingId($user->id);
+			return;
+		} else {
+			abort(500, 'wrong tokenid');
+		}
 	}
 
 }
