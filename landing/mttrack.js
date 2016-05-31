42180a233b12a6a8420ee62602bd98ade0e36c2f
Thanh Kieu <thanhpk@live.com>
2016-05-31 19:43:05 +0100

    change script for better get mt.actionid

--- landing/mttrack.js
+++ landing/mttrack.js
@@ -1,16 +1,28 @@
-var event, mt = { track:function(t,r){
+//very light script to load first
+var event,
+mt.track = function(t,r){
 	mt.rq.push(["track",t,r,new Date])
-}, identify:function(t){
+};
+mt.identify = function(t){
 	mt.rq.push(["identify",t])
-}, clear:function(){
-	mt.rq.push(["clear"])
-}, onready:function(){
-	if(event) mt.excute(event)
-},rq:[]
 }
-addEventListener("message", function(ev){
-	var origin = event.origin || event.originalEvent.origin
-	if (origin.split('/')[2] !== "meotrics.com") return;
-	event = ev
-	if(mt.excute) mt.excute(ev);
-}, false)
+mt.clear = function(){
+	mt.rq.push(["clear"])
+}
+mt.onready = function(){
+	if(event) mt.excute(event)
+}
+mt.rq = []
+}
+
+(function(){
+	addEventListener("message", function(ev){
+		var origin = event.origin || event.originalEvent.origin
+		if (origin.split('/')[2] !== "meotrics.com") return;
+		event = ev
+		if(mt.excute) mt.excute(ev);
+	}, false);
+
+	var s = getComputedStyle(document.getElementById('mt' + mt.appid), null);
+	mt.actionid = s['font-family'].substr(1, s.content.length - 2);
+})();
