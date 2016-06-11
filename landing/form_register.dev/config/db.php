cab2f9abe61b4f90d58cfc83f85b297d15412b73
Viet Le <ldviet92@gmail.com>
2016-06-12 01:13:12 +0700

    update account and file mysql

--- landing/form_register.dev/config/db.php
+++ landing/form_register.dev/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=form_register',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'apply',
+    'password' => 'apply',
     'charset' => 'utf8',
 ];
