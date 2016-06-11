3244dacf98605b71d32c7d64efbe9e24dc3afc36
Pham Kieu Thanh <thanhpk@live.com>
2016-06-12 00:29:51 +0700

    check paid search

--- core/module/referer.js
+++ core/module/referer.js
@@ -1,5 +1,59 @@
 "use strict";
 class RefererType {
+    constructor() {
+        this.paidSearchs = ["alexa.com",
+            "amazon.com",
+            "aol.com",
+            "askjeeves.com",
+            "biglobe.ne.jp",
+            "bizrate.com",
+            "btopenworld.com",
+            "comcast.net",
+            "cs.com",
+            "dealtime.com",
+            "dogpile.com",
+            "earthlink.com",
+            "excite.com",
+            "froogle.com",
+            "go.com",
+            "disney.com",
+            "infospace.com",
+            "libero.it",
+            "mamma.com",
+            "metacrawler.com",
+            "mysearch.com",
+            "myway.com",
+            "netscape.com",
+            "nifty.com",
+            "nytimes.com",
+            "searchalot.com",
+            "searchresult.net",
+            "sportsline.com",
+            "sympatico.ca",
+            "teoma.com",
+            "updated.com",
+            "washingtonpost.com",
+            "webcrawler.com",
+            "yahoo.co.jp",
+            "chip.de",
+            "dindel.se",
+            "evreka.se",
+            "gulasidorna.se",
+            "kvasir.dk",
+            "kvasir.no",
+            "luotain.com",
+            "onvista.de",
+            "passagen.se",
+            "sol.dk",
+            "sol.no",
+            "suomi24.fi",
+            "t-online.de",
+            "selfpedia.org"];
+    }
+    isPaidSearch(url, referer) {
+        for (var l of this.paidSearchs) {
+        }
+    }
     getRefType(referer, medium) {
         return "";
     }
--- core/module/referer.ts
+++ core/module/referer.ts
@@ -1,62 +1,74 @@
-﻿export class RefererType {
+﻿//const url = require('url');
+import * as url from 'url';
+export class RefererType {
 
-     private  paidSearchs = ["alexa.com",
-    "amazon.com",
-    "aol.com",
-    "askjeeves.com",
-    "biglobe.ne.jp",
-    "bizrate.com",
-    "btopenworld.com",
-    "comcast.net",
-    "cs.com",
-    "dealtime.com",
-    "dogpile.com",
-    "earthlink.com",
-    "excite.com",
-    "froogle.com",
-    "go.com",
-    "disney.com",
-    "infospace.com",
-    "libero.it",
-    "mamma.com",
-    "metacrawler.com",
-    "mysearch.com",
-    "myway.com",
-    "netscape.com",
-    "nifty.com",
-    "nytimes.com",
-    "searchalot.com",
-    "searchresult.net",
-    "sportsline.com",
-    "sympatico.ca",
-    "teoma.com",
-    "updated.com",
-    "washingtonpost.com",
-    "webcrawler.com",
-    "yahoo.co.jp",
-    "chip.de",
-    "dindel.se",
-    "evreka.se",
-    "gulasidorna.se",
-    "kvasir.dk",
-    "kvasir.no",
-    "luotain.com",
-    "onvista.de",
-    "passagen.se",
-    "sol.dk",
-    "sol.no",
-    "suomi24.fi",
-    "t-online.de",
-    "selfpedia.org"];
-     private isPaidSearch(url: string, referer: string): boolean {
-         for (var l of this.paidSearchs)
-         {
+	private paidSearchs = [
+				"alexa.com",
+				"amazon.com",
+				"aol.com",
+				"askjeeves.com",
+				"biglobe.ne.jp",
+				"bizrate.com",
+				"btopenworld.com",
+				"comcast.net",
+				"cs.com",
+				"dealtime.com",
+				"dogpile.com",
+				"earthlink.com",
+				"excite.com",
+				"froogle.com",
+				"go.com",
+				"disney.com",
+				"infospace.com",
+				"libero.it",
+				"mamma.com",
+				"metacrawler.com",
+				"mysearch.com",
+				"myway.com",
+				"netscape.com",
+				"nifty.com",
+				"nytimes.com",
+				"searchalot.com",
+				"searchresult.net",
+				"sportsline.com",
+				"sympatico.ca",
+				"teoma.com",
+				"updated.com",
+				"washingtonpost.com",
+				"webcrawler.com",
+				"yahoo.co.jp",
+				"chip.de",
+				"dindel.se",
+				"evreka.se",
+				"gulasidorna.se",
+				"kvasir.dk",
+				"kvasir.no",
+				"luotain.com",
+				"onvista.de",
+				"passagen.se",
+				"sol.dk",
+				"sol.no",
+				"suomi24.fi",
+				"t-online.de",
+				"selfpedia.org"
+	];
 
-         }
+	private isPaidSearch(url_string: string, referer: string): boolean {
+		var url_parts = url.parse(url_string, true);
 
-    }
+		var medium = url_parts.query.utm_medium;
+		if (medium == "cpc" || medium == "ppc" || medium == "paid-search" || medium == "Paid search" || medium == "paidsearch")
+			return true;
+	
+		 url_parts = url.parse(referer, true);
+		for (var l of this.paidSearchs) 
+			if (url_parts.hostname.endsWith(l)) 
+				return true;
+		
+		return false;
+	}
 
-    public getRefType(referer: string, medium: string): string {
-        return "";
-    }
+	public getRefType(referer: string, medium: string): string {
+				return "";
+	}
 }
