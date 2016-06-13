f2c83eacb29c8a83fdc8ddc23a49d01f8a9eac42
Pham Kieu Thanh <thanhpk@live.com>
2016-06-13 22:08:15 +0700

    refine dashboard

--- dashboard/resources/views/home.blade.php
+++ dashboard/resources/views/home.blade.php
@@ -5,6 +5,7 @@
 @section('script')
 	<script src="{{asset('/js/Chart.js')}}"></script>
 	<script>
+		console.log({!! json_encode($dashboard) !!});
 		onPageLoad(function () {
 			var $tp = $('#timepick');
 			var tp = $tp.dateRangePicker();
@@ -42,66 +43,24 @@
 			//tooltipTemplate: {{"<%= value %> %"}},
 		};
 
-		var retenratechart = new Chart($("#retenratechart"), {
+		var labels = [];
+		labels = {!!   json_encode($dashboard->labels) !!};
+		//
+		var totalrevenues = [];
+		totalrevenues = {!! json_encode($dashboard->revenues) !!};
+		//
+		var usergrowthrates = [];
+		usergrowthrates = {!! json_encode($dashboard->usergrowth_rates) !!};
+		//
+		var retenratechart = new Chart($("#revenuechart"), {
 			type: 'line',
 			data: {
-				labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+				labels: labels,
 				datasets: [
 					{
-						label: "My First dataset",
-					//	fill: false,
+						label: "Revenue",
 						lineTension: 0.5,
-
 						borderCapStyle: 'round',
-
-						borderDash: [],
-						borderDashOffset: 0.0,
-						borderJoinStyle: 'miter',
-						pointHoverRadius: 1,
-						borderWidth: 2,
-						pointHoverBackgroundColor: "rgba(75,192,192,1)",
-						pointHoverBorderColor: "rgba(220,220,220,1)",
-						pointHoverBorderWidth: 2,
-						pointRadius: 2,
-						pointHitRadius: 10,
-						fill: true,
-						borderColor: "#2F5CB0",
-						backgroundColor: "rgba(78,108,201,0.3)",
-						pointBorderColor: "#2F5CB0",
-						pointBackgroundColor: "white",
-						pointBorderWidth: 1,
-
-						data: [65, 59, 80, 81, 32, 12, 40]
-					}
-				]
-			},
-			options: {
-
-				legend: {display: false},
-				maintainAspectRatio: false,
-				responsive: true,
-				height: "240",
-				animation: false,
-				scales: {
-					yAxes: [{display: false}], xAxes: [{display: false}]
-				}
-			}
-		});
-
-		var totalrevenues =[];
-		{{"totalrevenues = " .json_encode($dashboard->total_revenues)}}
-
-		var trChart = new Chart($("#trchart"), {
-			type: 'line',
-			data: {
-				labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-				datasets: [
-					{
-						label: "My First dataset",
-						lineTension: 0.5,
-
-						borderCapStyle: 'round',
-
 						borderDash: [],
 						borderDashOffset: 0.0,
 						borderJoinStyle: 'miter',
@@ -131,7 +90,46 @@
 					yAxes: [{display: false}], xAxes: [{display: true}]
 				}
 			}
+		});
 
+		var trChart = new Chart($("#ugchart"), {
+			type: 'line',
+			data: {
+				labels: labels,
+				datasets: [
+					{
+						label: "User Growth Rate ",
+						lineTension: 0.5,
+						borderCapStyle: 'round',
+						borderDash: [],
+						borderDashOffset: 0.0,
+						borderJoinStyle: 'miter',
+						pointHoverRadius: 1,
+						borderWidth: 2,
+						pointHoverBackgroundColor: "rgba(75,192,192,1)",
+						pointHoverBorderColor: "rgba(220,220,220,1)",
+						pointHoverBorderWidth: 2,
+						pointRadius: 2,
+						pointHitRadius: 10,
+						fill: true,
+						borderColor: "#2F5CB0",
+						backgroundColor: "rgba(78,108,201,0.3)",
+						pointBorderColor: "#2F5CB0",
+						pointBackgroundColor: "white",
+						pointBorderWidth: 1,
+						data: usergrowthrates
+					}
+				]
+			},
+			options: {
+				maintainAspectRatio: false,
+				responsive: true,
+				legend: {display: false},
+				animation: false,
+				scales: {
+					yAxes: [{display: false}], xAxes: [{display: true}]
+				}
+			}
 		});
 
 		function drawVisitNumber() {
@@ -159,7 +157,7 @@
 		var visitChart = new Chart($("#visitchart"), {
 			type: 'doughnut',
 			data: {
-				labels: ["New visitor","Returning visitor"],
+				labels: ["New visitor", "Returning visitor"],
 				datasets: [
 					{
 						data: [{{$dashboard->n_new_visitor}}, {{$dashboard->n_returning_visitor == 0 ? 1 : $dashboard->n_returning_visitor}}],
@@ -185,7 +183,7 @@
 							<div class="row">
 
 								<div class="col-sm-6" style="font-size: 12px">
-									<h6 style="margin-bottom:0; margin-top:20px;  color: gray">TRAFFIC</h6>
+									<h6 style="margin-bottom:0; margin-top:20px;  color: gray">TODAY VISITOR</h6>
 									<div class=""><i class="fa fa-circle" style="color: #4E6CC9"></i> New visitor</div>
 									<div class="medium text-center" style="color: #4E6CC9">{{$dashboard->n_new_visitor}}</div>
 									<div style="margin-top: 10px"><i class="fa fa-circle" style="color: #8C8C8C"></i> Returning visitor
@@ -210,7 +208,7 @@
 					<div class="card" style="height: 70px">
 						<div class="" style="">
 							<div class="header text-center">
-								<h6 style="margin: 0; color: gray">NEW SIGNUP</h6>
+								<h6 style="margin: 0; color: gray">TODAY NEW SIGNUP</h6>
 							</div>
 							<div class="content text-center" style="padding-top: 0">
 								<span class="big">{{$dashboard->n_new_signup}}</span>
@@ -229,7 +227,7 @@
 						</div>
 						<div class="content">
 							<div style="height: 175px">
-								<canvas id="trchart" width="400" height="400"></canvas>
+								<canvas id="ugchart" width="400" height="400"></canvas>
 							</div>
 						</div>
 					</div>
@@ -241,10 +239,10 @@
 		<div class="col-sm-3 pl0">
 			<div class="card" style="background: #4E6CC9; color: white; height: 80px">
 				<div class="header text-center">
-					<h6 style="margin: 0; color: white">RETENSION RATE</h6>
+					<h6 style="margin: 0; color: white">TODAY RETENSION RATE</h6>
 				</div>
 				<div class=" pull-right mr">
-					<span class="big" style="position: absolute; bottom: 20px;right: 42px;"> %</span>
+					<span class="big" style="position: absolute; bottom: 20px;right: 42px;"> {{$dashboard->retention_rate}} %</span>
 				</div>
 			</div>
 		</div>
@@ -264,7 +262,7 @@
 					<h6 style="margin: 0; color: white">PURCHASE CONVERSION RATE</h6>
 				</div>
 				<div class="content text-center pull-right mr">
-					<span class="big" style="position: absolute; bottom: 20px;right: 42px;"> {{$dashboard->conversion_rates[ count ($dashboard->conversion_rates) -1]}}
+					<span class="big" style="position: absolute; bottom: 20px;right: 42px;"> {{$dashboard->conversion_rate}}
 						%</span>
 				</div>
 			</div>
@@ -287,17 +285,19 @@
 				<div class="col-md-12">
 					<div class="card " style="height: 240px;">
 						<div class="header">
-							<h6 class="pull-right" style="margin:0; color: gray">REVENUER PER CUSTOM <span class="medium"
-							                                                                               style="color: #4e6cc9">$59.00</span>
+							<h6 class="pull-right" style="margin:0; color: gray">
+								REVENUER PER CUSTOM
+								<span class="medium" style="color: #4e6cc9">$ {{$dashboard->revenue_per_customer}}</span>
 							</h6>
 
-							<h6 style="margin:0; color:gray">TOTAL REVENUE <span class="medium"
-							                                                     style="color: #4e6cc9">$46, 238.00</span></h6>
-
+							<h6 style="margin:0; color:gray">
+								TOTAL REVENUE
+								<span class="medium" style="color: #4e6cc9">$ {{$dashboard->total_revenue}}</span>
+							</h6>
 						</div>
 						<div class="content">
 							<div style="height: 190px">
-								<canvas id="retenratechart" width="100%" height="100%"></canvas>
+								<canvas id="revenuechart" width="100%" height="100%"></canvas>
 							</div>
 						</div>
 					</div>
@@ -314,7 +314,8 @@
 						</div>
 						<div class="content text-center pull-right mr">
 							<h6 style="position: absolute; bottom: 20px;right: 30px; text-align: right;width: 80%;margin-top: 0;">
-								Woman Fashion & Accessories</h6>
+							{{$dashboard->most_popular_category or "N/A"}}
+							</h6>
 						</div>
 					</div>
 				</div>
@@ -328,7 +329,8 @@
 						</div>
 						<div class="content text-center pull-right mr">
 							<h6 style="position: absolute; bottom: 20px;right: 30px; text-align: right;width: 80%;margin-top: 0;">
-								Facebook</h6>
+								{{$dashboard->highest_revenue_campaign or "N/A" }}
+							</h6>
 						</div>
 					</div>
 				</div>
@@ -342,15 +344,14 @@
 						</div>
 						<div class="content text-center pull-right mr">
 							<h6 style="position: absolute; bottom: 20px;right: 30px; text-align: right;width: 80%;margin-top: 0;">
-								Organic search</h6>
+								{{$dashboard->most_effective_ref or "N/A"}}
+							</h6>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-
-
 @endsection
 @section('additional')
 
@@ -360,5 +361,5 @@
 	<li>
 		<input style="width: 220px;margin-top: 7px;" class="form-control mr" id="timepick">
 	</li>
-	@endsection
+@endsection
 
