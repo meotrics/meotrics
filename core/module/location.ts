82cb1eb785646c1c99d5d2389d651c983a346812
Pham Kieu Thanh <thanhpk@live.com>
2016-06-24 02:08:25 +0700

    use maxmind for location

--- core/module/location.ts
+++ core/module/location.ts
@@ -1,31 +1,27 @@
 ﻿import * as request from 'request';
 import * as mongodb from 'mongodb';
+var maxmind = require('maxmind');
 
+class Location {
+	public constructor(public city: string, public country: string) { }
+}
 export class LocationMgr {
 	private collection: mongodb.Collection;
+	private lookup;
 	public constructor(private db: mongodb.Db, private prefix: string) {
 		this.collection = db.collection(prefix + "ip");
+
+		this.lookup = maxmind.open('./asset/GeoLite2-City.mmdb', {
+			cache: {
+				max: 1000, // max items in cache
+				maxAge: 1000 * 60 * 60 // life time in milliseconds
+			}
+		});
 	}
 
-	public parse(ip: string, callback: (data) => void) {
-		var me = this;
-		// 1 find in database
-		me.collection.find({ ip: ip }).limit(1).toArray(function (err, res) {
-			if (err) throw err;
-			if (res.length !== 0)
-				return callback(res[0]);
-
-			// 2 using the api
-			request('http://ipinfo.io/' + ip, function (err, res, body)
-			{
-				if (!err && res.statusCode == 200) {
-					callback(body);
-					body = JSON.parse(body);
-					me.collection.insertOne(body, function (err, ret) {
-						if (err) throw err;
-					});
-				}
-			});
-		});
+	public parse(ip: string, callback: (data: Location ) => void) {
+		var loc = this.lookup.get(ip);
+		console.log(loc.city.names.en, loc.country.names.en);
+		return callback(new Location(loc.city.names.en, loc.country.names.en));
 	}
 }
