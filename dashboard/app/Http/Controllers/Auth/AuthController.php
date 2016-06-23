715f07640be588fb953928d2dc8551739e291eaf
haiha <haihabk.nguyen@gmail.com>
2016-06-24 01:19:29 +0700

    Update app management

--- dashboard/app/Http/Controllers/Auth/AuthController.php
+++ dashboard/app/Http/Controllers/Auth/AuthController.php
@@ -355,7 +355,11 @@ class AuthController extends Controller
 			}
 
 			\Auth::loginUsingId($userid);
-			return "/app";
+                        $apps = DB::table('apps')->join('user_app', 'apps.id', '=', 'user_app.appid')
+			->where('user_app.userid', $userid)->get();
+                        $appcode = isset($apps[0]) ? $apps[0]->code : '';
+                        
+			return "/app/".$appcode;
 		} else {
 			return view("errors.500", ['error'=>'wrong tokenid']);
 		}
