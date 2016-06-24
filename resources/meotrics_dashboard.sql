1a25f56319ad88ea8bb109cbc863b1ec03b86aa5
Pham Kieu Thanh <thanhpk@live.com>
2016-06-24 20:31:41 +0700

    fix db

--- resources/meotrics_dashboard.sql
+++ resources/meotrics_dashboard.sql
@@ -1,31 +1,63 @@
-SET FOREIGN_KEY_CHECKS=0;
+-- MySQL dump 10.13  Distrib 5.6.30, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: meotrics
+-- ------------------------------------------------------
+-- Server version	5.6.30-0ubuntu0.14.04.1
 
--- ----------------------------
--- Table structure for `apps`
--- ----------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `apps`
+--
+
 DROP TABLE IF EXISTS `apps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `apps` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `ownerid` int(11) NOT NULL,
   `code` varchar(30) NOT NULL,
   `backendid` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `codename` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of apps
--- ----------------------------
-INSERT INTO `apps` VALUES ('1', 'Xa lo van su', '1', 'XALOVANSU', '');
-INSERT INTO `apps` VALUES ('2', 'Speedy Apply', '1', 'SPEEDYAPPLY', '');
-INSERT INTO `apps` VALUES ('3', 'Speedy Access', '1', 'SPEEDYACCESS', '');
-INSERT INTO `apps` VALUES ('4', 'Okr', '1', 'OKR', '');
+--
+-- Table structure for table `user_app`
+--
 
--- ----------------------------
--- Table structure for `users`
--- ----------------------------
+DROP TABLE IF EXISTS `user_app`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_app` (
+  `appid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `can_perm` tinyint(1) NOT NULL,
+  `can_struct` tinyint(1) NOT NULL,
+  `can_report` tinyint(1) NOT NULL,
+  PRIMARY KEY (`appid`,`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
 DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
@@ -41,34 +73,21 @@ CREATE TABLE `users` (
   `updated_at` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `remember_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `verified` int(4) NOT NULL DEFAULT 0,
+  `verified` int(4) NOT NULL DEFAULT '0',
+  `resetpwhash` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `password_reset_token` (`password_reset_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', '', '$2y$10$vlR4xsKws521k0JDWkW73ezOHBXDGZePi048qkiGfCklW/KHsD9KK', null, 'thanh@gmail.com', null, null, null, null, '10', '2016', '2016', 'Thanh', null, 1);
-INSERT INTO `users` VALUES ('2', '', '$2y$10$xaERgAC4H.p5BLE1efhoWeDmzTNyFaS4D0pKbhmHET1AwA31sIYF2', null, 'van@gmail.com', null, null, null, null, '10', '2016', '2016', 'Hai van', null, 1);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- ----------------------------
--- Table structure for `user_app`
--- ----------------------------
-DROP TABLE IF EXISTS `user_app`;
-CREATE TABLE `user_app` (
-  `appid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `can_perm` tinyint(1) NOT NULL,
-  `can_struct` tinyint(1) NOT NULL,
-  `can_report` tinyint(1) NOT NULL,
-  PRIMARY KEY (`appid`,`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user_app
--- ----------------------------
-INSERT INTO `user_app` VALUES ('1', '1', '1', '1', '1');
-INSERT INTO `user_app` VALUES ('4', '1', '1', '1', '1');
-INSERT INTO `user_app` VALUES ('4', '2', '0', '1', '1');
+-- Dump completed on 2016-06-16  0:12:13
