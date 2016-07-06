4525fd8daea0ed565295be95594abde9b3ebe427
Pham Kieu Thanh <thanhpk@live.com>
2016-07-06 19:44:18 +0700

    secure home controller

--- dashboard/app/Http/Controllers/HomeController.php
+++ dashboard/app/Http/Controllers/HomeController.php
@@ -19,13 +19,14 @@ class HomeController extends Controller
 
 	public function postCurrentapp(Request $request, $appid)
 	{
-        $request;
+        if(Access::can_view($request->user()->id, $appid) == false) abort(500,'Permission Denied');
 		$response = new Response();
 		return $response->withCookie(cookie('currentappid', $appid, 2147483647, '/' . $appid . '/'));
 	}
 
 	public function postCurrenttime(Request $request, $appid)
 	{
+ if(Access::can_view($request->user()->id, $app_id) == false) abort(500,'Permission Denied');
 		$response = new Response();
 		$st = $request->input('startTime', null);
 		$et = $request->input('endTime', null);
