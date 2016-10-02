cb7cbbf388d99b69db95e65643221bf3a8a970fb
Viet Le <ldviet92@gmail.com>
2016-10-02 16:59:37 +0700

    remove kissmetrics

--- dashboard/resources/views/layout/main.blade.php
+++ dashboard/resources/views/layout/main.blade.php
@@ -27,19 +27,6 @@
 	@yield('style')
 	<link href="{{asset('css/app.css')}}" rel="stylesheet"/>
 	@yield('header-script')
-	<script type="text/javascript">var _kmq = _kmq || [];
-		var _kmk = _kmk || '517e9b1f300005861a6db4b27428d66c1902ad70';
-		function _kms(u){
-			setTimeout(function(){
-				var d = document, f = d.getElementsByTagName('script')[0],
-						s = d.createElement('script');
-				s.type = 'text/javascript'; s.async = true; s.src = u;
-				f.parentNode.insertBefore(s, f);
-			}, 1);
-		}
-		_kms('//i.kissmetrics.com/i.js');
-		_kms('//scripts.kissmetrics.com/' + _kmk + '.2.js');
-	</script>
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -54,7 +41,6 @@
 			}
 		?>
     ga('send', 'pageview');
-
 	</script>
 
 </head>
