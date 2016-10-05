bee69fa42ec34e1720593d9bcf795c747b3e0255
Tuan <hoangminhtuan.hust@gmail.com>
2016-10-05 21:14:30 +0700

    clear code

--- core/routes/actionType/getSpecific.js
+++ core/routes/actionType/getSpecific.js
@@ -11,43 +11,49 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get a specific action type
-router.get('/action-type/:appid/:_id', validate, function(req, res, next){
+router.get('/action-type/:appid/:_id', validate, handleMoreFileds, function(req, res, next) {
+    const appid = req.params.appid;
+    const _id = req.params._id;
+
+    const query = {
+        _appid: appid,
+        _id: new mongodb.ObjectID(_id)
+    };
+
+    const projection = {};
     const defaultField = [
         'name',
         '_id',
     ];
-
-    const appid = req.params.appid;
-    const _id = req.params._id;
-
-    const projection = {};
-    const totalFields = defaultField.concat(req.moreFields);
-    totalFields.forEach(v => {
+    defaultField.concat(req.moreFields).forEach(v => {
         projection[v] = 1;
     });
-    const options = {};
+
+    const options = {
+
+    };
 
     const collection = config.mongod.prefix + consts.ACTION_TYPE_COLLECTION;
 
-    globalVariables.get('db').collection(collection).findOne({
-        _appid: appid,
-        _id: new mongodb.ObjectID(_id)
-    }, projection, options, (err, r) => {
-        if(err) {
-            return next(err);
-        }
+    globalVariables
+        .get('db')
+        .collection(collection)
+        .findOne(query, projection, options, (err, r) => {
+            if(err) {
+                return next(err);
+            }
 
-        console.log(r);
-        res.json({
-            ec: consts.CODE.SUCCESS,
-            data: r
+            console.log(r);
+            res.json({
+                ec: consts.CODE.SUCCESS,
+                data: r
+            });
         });
-    })
 });
 
 // Middleware for checking data
 function validate(req, res, next) {
-    console.log('action-type:get-specific:', req.params, req.query);
+    console.log(req.params, req.query);
 
     const _id = req.params._id;
 
@@ -60,6 +66,10 @@ function validate(req, res, next) {
         return next('route');
     }
 
+    next();
+}
+
+function handleMoreFileds(req, res, next) {
     let moreFields = [];
     let extras = req.query.extras;
     if(!_.isUndefined(extras)) {
