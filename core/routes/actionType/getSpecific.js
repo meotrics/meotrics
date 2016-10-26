1fbf083f184fcee7538bf8ff70eb13ecd9d70299
Hoang Tuan <tuanhm@gviet.vn>
2016-10-26 10:11:07 +0700

    add get detail action type

--- core/routes/actionType/getSpecific.js
+++ core/routes/actionType/getSpecific.js
@@ -9,80 +9,91 @@ const validator = require('validator');
 const async = require('async');
 const mongodb = require('mongodb');
 const router = express.Router();
+let util = require('util');
+
+let allFields = [
+    '_id',
+    'codename',
+    'name',
+    'desc',
+    'deftrendfields',
+    'deftrendobjects',
+    'fields',
+    '_appid',
+    '_ctime'
+];
+
+let defaultFields = [
+    'fields'
+];
+
 
 // Get a specific action type
-router.get('/action-type/:appid/:_id', validate, handleMoreFileds, function(req, res, next) {
-    const appid = req.params.appid;
-    const _id = req.params._id;
+router.get('/action-type/:_appid/:_typeid',
+    validate,
+    getConverter,
+    handleMoreFileds,
+    function(req, res, next) {
+        let _appid = req.params._appid;
+        let _typeid = req.params._typeid;
+        let collection = config.mongod.prefix + consts.ACTION_TYPE_COLLECTION;
 
-    const query = {
-        _appid: appid,
-        _id: new mongodb.ObjectID(_id)
-    };
+        let query = {
+            [req.meotrics_converters['_appid']]: _appid,
+            [req.meotrics_converters['codename']]: _typeid
+        };
 
-    const projection = {};
-    const defaultField = [
-        'name',
-        '_id',
-    ];
-    defaultField.concat(req.moreFields).forEach(v => {
-        projection[v] = 1;
-    });
-
-    const options = {
-
-    };
-
-    const collection = config.mongod.prefix + consts.ACTION_TYPE_COLLECTION;
-
-    globalVariables
-        .get('db')
-        .collection(collection)
-        .findOne(query, projection, options, (err, r) => {
-            if(err) {
-                return next(err);
-            }
-
-            console.log(r);
-            res.json({
-                ec: consts.CODE.SUCCESS,
-                data: r
-            });
+        let projection = {};
+        req.meotrics_moreFields.forEach(field => {
+            projection[req.meotrics_converters[field]] = 1;
         });
-});
+
+        let options = {};
+
+        globalVariables
+            .get('db')
+            .collection(collection)
+            .findOne(query, projection, options, (err, result) => {
+                if(err) {
+                    return res.json({
+                        ec: consts.CODE.ERROR,
+                        error: util.inspect(err)
+                    });
+                }
+
+                res.json({
+                    ec: consts.CODE.SUCCESS,
+                    data: result
+                });
+            });
+    });
 
 // Middleware for checking data
 function validate(req, res, next) {
-    console.log(req.params, req.query);
+    return next();
+}
 
-    const _id = req.params._id;
-
-    if(!validator.isMongoId(_id)) {
-        res.json({
-            ec: consts.CODE.WRONG_PARAM,
-            reason: "_id must be an object id"
-        });
-
-        return next('route');
-    }
-
-    next();
+function getConverter(req, res, next) {
+    globalVariables.get('converter').toIDs(allFields, ids => {
+        req.meotrics_converters = ids;
+        return next();
+    });
 }
 
 function handleMoreFileds(req, res, next) {
-    let moreFields = [];
-    let extras = req.query.extras;
-    if(!_.isUndefined(extras)) {
-        let temp = extras.split(',');
-        temp.forEach(v => {
-            if(validator.isLength(v, 1)) {
-                moreFields.push(v);
-            }
-        });
-    }
+    let more = req.query.more || '';
+    let moreFields = more.split(',');
 
-    req.moreFields = moreFields;
-    next();
+    let temp = [];
+    moreFields.forEach(field => {
+        if( (allFields.indexOf(field) !== -1) && (defaultFields.indexOf(field) === -1) ) {
+            temp.push(field);
+        }
+    });
+
+    req.meotrics_moreFields = temp.concat(defaultFields);
+
+    return next();
 }
 
 module.exports = function (app) {
